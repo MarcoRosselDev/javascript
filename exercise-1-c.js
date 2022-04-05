9942f96f2395346fb850d80e7e9fef0c480719ef
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-05 19:26:12 -0400

    exercise 1-c

---
+++ exercise-1-c.js
@@ -0,0 +1,14 @@
+//exercise 1 :
+
+//      a
+//-dejar pasar solo a los mayores de 18
+//-el primero que entre después de las 2 am no paga
+
+//      b
+//-crear mini-sistema para registrar presentes (P)
+// y ausentes (A)
+//-pasados 30 dias mostrar su situación final del alumno 
+//-maximo de 10% de ausencias.
+
+//      c
+//-crear calculadora que nos simplifique el trabajo
