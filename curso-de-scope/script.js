0a9f8dbcd5317a36b0867db54924ddbd8e843be4
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-11 11:14:06 -0400

    class 2

---
+++ curso-de-scope/script.js
@@ -0,0 +1,9 @@
+var hello = 'wsp dude';
+let world = "hello world";
+const helloWorld = "hello world¡¡¡";
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
