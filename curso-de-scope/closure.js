f03ffa25ff1dc28f2e2b4f105c69589e2aea394a
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-11 16:34:42 -0400

    profundizando en clousure(cierre)

---
+++ curso-de-scope/closure.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+const capaExtra = () => {
+
+    //primero entendamos el nucleo de la suma y posterior acumulacion
+    var accumulation = 0;  // comienza declarando = 0
+    const caja = (dinero) => { // caja = dinero que se le agregara
+                                // y dinero es un numero cualquiera
+        accumulation += dinero; //más el acumulado
+        console.log(accumulation);//imprimir para ver el proceso.
+    }
+    // caja(0); //0   
+    // caja(3); //3
+    // caja(4); //7
+    // caja(6); //13
+    // caja(2); //15
+    // caja(4); //19
+    // caja(8); //27
+    //asta aqui esta bien pero esta mal visto cambiar variables globales asi es que es mejor meterlas dentro de otra capa
+    return caja;
+}
+
+let loQueSea = capaExtra(); //no entiendo este paso
+
+loQueSea(3);
+loQueSea(4);
+loQueSea(6);
+loQueSea(2);
+loQueSea(4);
+loQueSea(9);
+
+
