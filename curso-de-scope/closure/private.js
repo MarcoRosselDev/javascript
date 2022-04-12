785895b8e921798becf274286aa3562a8ed9324c
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-12 08:35:30 -0400

    class 8

---
+++ curso-de-scope/closure/private.js
@@ -0,0 +1,20 @@
+//ejecutar la logica
+
+//tener metodos internos con los cuales podremos obtener la informacion de una variable o asignarle algun valor a esta variable pero nunca poder hacerlo desde afuera.
+
+const persona = () => {
+    var guardarNombre = "nombre";
+    return {
+        dameElNombre: () => {
+            return guardarNombre;
+        },      //ojo esta coma. con ; marca error y sin nada tmbn
+        escribirNombre: (name) => {
+            guardarNombre = name;
+        },
+    };
+};
+
+nuevaPersona = persona();
+console.log(nuevaPersona.dameElNombre());
+nuevaPersona.escribirNombre("lushito");
+console.log(nuevaPersona.dameElNombre());
