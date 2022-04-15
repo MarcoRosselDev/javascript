e0b0f0d7acd19ad9e389d6618033a171265b7192
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-15 14:49:55 -0400

    axample uber whit ecmascript 6 successful

--- curso-de-POO/js-ECMAscript-6/index.js
+++ curso-de-POO/js-ECMAscript-6/index.js
@@ -1,3 +1,11 @@
 var car = new Car("AB324", new Account("Marco Rossel", "GJ767"))
 car.passenger = 4;
 car.printDataCar();
+
+// para utilizar una de las clases y crear un objet, por ejemplo uberx,
+// no olvidar declarar la clase en el archivo idex.html
+
+
+var uberX = new UberX("BB344", new Account("Juan Soto", "HH900"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
