a8f876fa2f9d34e1c71caa4f1fc38dd0b5ffc610
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-19 18:40:15 -0400

    playing whit a object

---
+++ js-poo/protoripos/main.js
@@ -0,0 +1,12 @@
+const natalia = {
+    name : "natalia",
+    age : 20,
+    cursosAprobados: [
+        "curso x",
+        "curso y",
+    ],
+};
+
+// hacer que natalia apruebe otro curso
+
+natalia.cursosAprobados.push("curso z");
