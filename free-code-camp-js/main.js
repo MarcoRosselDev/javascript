f74183b00abae7ce8c554ba784a79b32c2495287
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-20 15:39:12 -0400

    class 4 fcc-js

--- free-code-camp-js/main.js
+++ free-code-camp-js/main.js
@@ -52,3 +52,17 @@ tratará como si fuera 5.
 
 */
 
+/* ASIGNAR EL VALOR DE UNA VARIABLE A OTRA
+
+Después de asignar un valor a una variable mediante el operador de
+asignación, puede asignar el valor de esa variable a otra variable 
+mediante el operador de asignación.
+
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar
+
+Lo anterior declara una myVar variable sin valor, luego le asigna el valor 5. A continuación, se declara una variable nombrada myNum sin valor. Ahora, myNum también tiene el valor 5.
+
+*/
