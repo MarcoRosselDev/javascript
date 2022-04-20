e4e740a5cb493c0c44746918ce2cd1da6b522529
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-20 15:54:46 -0400

    class 6 fcc-js

--- free-code-camp-js/main.js
+++ free-code-camp-js/main.js
@@ -78,3 +78,18 @@ de 3.
 
 
 */
+
+/* class 6 DECLARAR VARIABLES DE CADENA
+
+Previamente usó el siguiente código para declarar una variable:
+
+var myName;
+
+Pero también puede declarar una variable de cadena como esta:
+
+var myName = "your name";
+
+"your name" se llama literal de cadena. Un literal de cadena, es una serie de cero o más caracteres entre comillas simples o dobles.
+
+
+*/
