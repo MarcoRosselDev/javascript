c64455949cb234b331040b82faa3b59d114d4a3d
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-20 02:44:54 -0400

    2.1 fcc

--- free-code-camp-js/main.js
+++ free-code-camp-js/main.js
@@ -2,3 +2,28 @@
 
 /* This is a 
 multi-line comment */
+
+/* javascript proporciona 8 tipos de datos
+
+    1 string
+    2 symbol
+    3 bigint
+    4 number
+    5 object
+    6 undefined
+    7 null
+    8 boolean
+    
+la computadora distingue entre números como 23 44 y strings como 
+"dog", "cats 3241 "... que son colecciones de caracteres.
+
+las computadoras pueden realizar operaciones matemáticas en números,
+pero no en una cadena.
+
+las variables permiten que las computadoras almacenen y manipulen 
+datos de form adinámica. Lo hacen mediante el uso de una "etiqueta"
+para señalar los datos en lugar de utilizar los datos en sí. Cualquiera de los ocho tipos de datos puede almacenarse en una vatiable.
+
+
+
+    */
