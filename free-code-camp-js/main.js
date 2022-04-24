2b72bc33e75f1f7c2235db8137af620503b1c185
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-24 00:32:58 -0400

    class 44 fcc-js

--- free-code-camp-js/main.js
+++ free-code-camp-js/main.js
@@ -620,7 +620,23 @@ const myData = myArray[2][1];
 console.log(myData);
 
 */
-/* */
+/* class 44 MANIPULAR ARREGLOS CON PUSH()
+
+una manera fácil de agregar datos al final de una matriz es a través
+de la push() función.
+.push() toma uno o más parámetros y los "empuja" al final de la matriz.
+
+ejemplo:
+
+const arr1 = [1, 2, 3];
+arr1.push(4);
+
+const arr2 = ["Stimpson", "J", "cat"];
+arr2.push(["happy", "joy"]);
+
+arr1ahora tiene el valor [1, 2, 3, 4]y arr2tiene el valor ["Stimpson", "J", "cat", ["happy", "joy"]].
+
+*/
 /* */
 /* */
 /* */
