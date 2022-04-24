2958089a48349973bb11db707a7978f9423c9e6a
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-24 08:14:12 -0400

    class 46 fcc-js

--- free-code-camp-js/main.js
+++ free-code-camp-js/main.js
@@ -679,16 +679,7 @@ const myArray = [["John", 23], ["cat", 2]];
 const removedFromMyArray = myArray.pop();
 
 */
-// Setup
-const myArray = [["John", 23], ["cat", 2]];
-
-// Only change code below this line
-
-const removedFromMyArray = myArray.pop();
-
-console.log(myArray);
-console.log(removedFromMyArray);
-/* */
+/* class 46*/
 /* */
 /* */
 /* */
