63473917fd62c8afd03b5405c4c31c160f0a1e07
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-25 23:41:35 -0400

    class 53 fcc-js

--- free-code-camp-js/2.js
+++ free-code-camp-js/2.js
@@ -99,3 +99,48 @@ function fun2() {
 }
 
 */
+/* class 53 ÁMBITO LOCAL Y FUNCIONES
+
+Las variables que se declaran dentro de una función, así como los parámetros de la función, tienen alcance local . Eso significa que solo son visibles dentro de esa función.
+
+Aquí hay una función myTestcon una variable local llamada loc.
+
+function myTest() {
+  const loc = "foo";
+  console.log(loc);
+}
+
+myTest();
+console.log(loc);
+
+La myTest() llamada a la función mostrará la cadena foo en la consola. La console.log(loc) línea (fuera de la myTest función) arrojará un error, ya loc que no está definida fuera de la función.
+
+
+*/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
