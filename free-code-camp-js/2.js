5f95b158d3369392820c1a1fc39ad4947dacd8aa
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-27 08:05:06 -0400

    class 67 fcc-js exercise-1

--- free-code-camp-js/2.js
+++ free-code-camp-js/2.js
@@ -663,6 +663,26 @@ ejemplo;
 5   < 5 // false
 3   < 2 // false
 '8' < 4 // false
+-------------------------------------------------------------------
+ejericio;
+
+agregue el operador menor que a las líneas indicadas para que las declaraciones de retorno tengan sentido.
+
+solución;
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
 
 */
 /* */
