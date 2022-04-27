ee6b6b4b4e40afd2e3dcb5f0094aa916993d93e5
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-26 22:05:42 -0400

    class 60 fcc-js

--- free-code-camp-js/2.js
+++ free-code-camp-js/2.js
@@ -376,7 +376,38 @@ function trueOrFalse(wasThatTrue) {
 }
 
 */
-/* */
+/* class 60 COMPARACIÓN CON EL OPERADOR DE IGUALDAD
+
+hay muchos operadores de comparación en javascript. 
+todos estos operadores devuelven un valor booleano true o false.
+el operador más básico es el operador de igualdad ==. 
+el operador de igualdad compara dos valores y devuelve true si son 
+equivalentes o false si no.
+tener en cuenta que la igualdad es diferente de la asignación =,
+que asiga el valor a la derecha del operador a una variable a la 
+izquierda.
+
+function equalityTest(myVal) {
+  if (myVal == 10) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+si myVal es igual a 10, el operador de igualdad devuelve true, 
+por lo que el código entre llaves se ejecutará y la función devolverá
+Equal.
+de lo contrario, la funición devolverá Not Equal.
+para que javascript compare dos tipos de datos diferentes (por ejemplo,
+  numbers and strings) deve convertir un tipo en otro.
+  esto se conoce como tipo de coerción. sin embargo, una vez que lo hace, puede comparar términos de la siguiente manera:
+
+1   ==  1  // true
+1   ==  2  // false
+1   == '1' // true
+"3" ==  3  // true
+
+*/
 /* */
 /* */
 /* */
