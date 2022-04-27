d818ea0cb65749356d5aae9fd9fb06d07251050f
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-27 08:20:47 -0400

    class 69 fcc-js

--- free-code-camp-js/2.js
+++ free-code-camp-js/2.js
@@ -724,7 +724,30 @@ function testLessOrEqual(val) {
 testLessOrEqual(10);
 
 */
-/* */
+/* class 69 COMPARACIONES CON EL OPERADOR LÓGICO Y
+
+a veces necesitaremos probar más de una vosa a la vez. el operador 
+lógico and && devuelve true si y solo si los operados a la izquierda
+y a la derecha son verdaderos.
+se podría lograr el mismo efecto anidando una sentencia if dentro
+de otra if:
+
+if (num > 5) {
+  if (num < 10) {
+    return "Yes";
+  }
+}
+return "No";
+
+solo regresará yes si num es mayor que 5 y menor que 10.
+la misma lógica se puede esrcibir como:
+
+if (num > 5 && num < 10) {
+  return "Yes";
+}
+return "No";
+
+*/
 /* */
 /* */
 /* */
