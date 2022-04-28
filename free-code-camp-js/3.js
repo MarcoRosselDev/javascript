752baf0114be1db217444421f7d821478c001ac4
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-28 03:05:13 -0400

    class 71 fcc-js exercise-1

--- free-code-camp-js/3.js
+++ free-code-camp-js/3.js
@@ -10,6 +10,28 @@ if (num > 10) {
 } else {
   return "10 or Less";
 }
+-------------------------------------------------------------------
+ejercicio
+
+combina las if ceclaraciones en una sola if/else declaración
+
+solución;
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
 
 */
 /* */
