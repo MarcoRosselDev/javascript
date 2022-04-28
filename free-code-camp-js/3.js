aff77530987aa830dc149d45dd4dc7ca3bc20e05
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-28 11:46:01 -0400

    class 76 fcc-js exercise-1

--- free-code-camp-js/3.js
+++ free-code-camp-js/3.js
@@ -256,6 +256,44 @@ en case los valores se prueban con igualdad estricta ===.
 la break dice a javascript que deje de ejecutar declaraciones.
 si break se omite, se ejecutará la siguiente instruicción.
 
+--------------------------------------------------------------
+ejercicio
+
+escriba una declaración de cambio que pruebe val y establezca answer
+las siguientes condiciones;
+
+1-alpha
+2-beta
+3-gamma
+4-delta
+
+solución;
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
 */
 /* */
 /* */
