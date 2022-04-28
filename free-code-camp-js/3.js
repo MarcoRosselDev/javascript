fa9277d91b5a352e9e191f66ecba66ed98079897
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-28 15:24:49 -0400

    class 78 fcc-js exercise-1

--- free-code-camp-js/3.js
+++ free-code-camp-js/3.js
@@ -376,6 +376,47 @@ switch(val) {
 }
 
 los casos 1, 2 y 3 producirán todos el mismo resultado.
+-------------------------------------------------------------------
+ejercico
+
+escriba una declaración de cambio para establecer answer los siguientes
+rangos:
+
+1-3-Low
+4-6-Mid
+7-9-High
+
+nota----deberá tener una declaración case para número en el rango-
+
+solución;
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val){
+  case 1:
+  case 2:
+  case 3:
+    answer = "Low";
+    break;
+  case 4:
+  case 5:
+  case 6:
+    answer = "Mid";
+    break;
+  case 7:
+  case 8:
+  case 9:
+    answer = "High";
+    break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
 
 */
 /* */
