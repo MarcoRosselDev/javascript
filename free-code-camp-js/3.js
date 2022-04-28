488e20cc7b4d7da4ccd1d73977a967a85f06ff9e
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-28 03:01:03 -0400

    class 71 fcc-js

---
+++ free-code-camp-js/3.js
@@ -0,0 +1,43 @@
+/* class 71 introdicción a las sentencias else
+
+cuando una condición para un if es verdadera, se ejecuta el bloque 
+de código que la sigue. ¿ qué pasa cuando esa condición es falsa ?
+normalmente no pasaría nada.    con una else declaración, se puede
+ejecutar un bloque de código alternativo.
+
+if (num > 10) {
+  return "Bigger than 10";
+} else {
+  return "10 or Less";
+}
+
+*/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
