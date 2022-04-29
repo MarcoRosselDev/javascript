8c9803bc3902b2de634349c93466037be6c10719
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-29 03:48:49 -0400

    class 79 fcc-js exercise-1

--- free-code-camp-js/3.js
+++ free-code-camp-js/3.js
@@ -447,6 +447,42 @@ switch(val) {
     answer = "c";
 }
 
+---------------------------------------------------------------
+ejercicio;
+
+cambie las declaraciones encadenadas if /  else if 
+en una declaración switch.
+
+solución;
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+ 
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
 */
 /* */
 /* */
