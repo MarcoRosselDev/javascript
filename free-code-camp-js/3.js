d5f2dbc390bb86726dc6aa5bd2d094ddc14baf41
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-30 12:47:11 -0400

    class 93 fcc-js

--- free-code-camp-js/3.js
+++ free-code-camp-js/3.js
@@ -1310,7 +1310,32 @@ const myMusic = [
 ];
 
 */
-/* */
+/* class 93 acceso a objetos anidados
+
+se puede acceder a las subpropiedades de los objetos encadenando la 
+notación de punto o corchete.
+aquí hay un objeto anidado;
+
+const ourStorage = {
+  "desk": {
+    "drawer": "stapler"
+  },
+  "cabinet": {
+    "top drawer": { 
+      "folder1": "a file",
+      "folder2": "secrets"
+    },
+    "bottom drawer": "soda"
+  }
+};
+
+ourStorage.cabinet["top drawer"].folder2;
+ourStorage.desk.drawer;
+
+ourStorage.cabinet["top drawer"].folder2 sería la cadena secrets,
+y ourStorage.desk.drawer sería la cadena stapler.
+
+*/
 /* */
 /* */
 /* */
