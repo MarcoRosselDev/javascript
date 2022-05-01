ed17e0fbddbcb0f644943ab477252eedb4a4fb14
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-01 08:06:19 -0400

    class 95 fcc-js

--- free-code-camp-js/4.js
+++ free-code-camp-js/4.js
@@ -84,7 +84,32 @@ const myPlants = [
 const secondTree = myPlants[1].list[1];
 
 */
-/* */
+/*class 95 colección de discos
+
+se te da un objeto literal que representa una parte de tu colección
+de álbumes musicales.
+cada álbum tiene un número de id único como clave y varias otras 
+propiedades. no todos los álbumes tienen una información completa.
+
+empieza con una función updataRecords la cual toma un objeto literal,
+records, que contiene el álbum musical de la colección, un id, prop
+(como artist o tracks), y value.
+completa la función usando las reglas siguientes para modificar el 
+objeto pasado a la función.
+
+--- tu cunnción siempre debe devolover el objeto de colección de 
+    registros completo.
+--- si prop no es tracks y value no es una cadena vacía, actualiza o 
+    establece la propiedad prop del álbum a value.
+--- si prop es tracks pero el álbum no tiene una propiedad tracks ,crea
+    un arreglo vacío y agrégale value a él.
+--- si prop es trcks y value no es una cadena vacía, agrega value al 
+    final del arreglo de tracks existentes del álbum.
+--- si value es una cadena vacía, elimina esa propiedad prop del álbum.
+
+nota-----se usa una copia del objeto recordCollection para las pruebas.
+
+*/
 /* */
 /* */
 /* */
