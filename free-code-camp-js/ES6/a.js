b86864ed1f9e30ca575025c74a768eaefa750c13
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-04 19:33:31 -0400

    class 11 fcc-js ES6 exercise

--- free-code-camp-js/ES6/a.js
+++ free-code-camp-js/ES6/a.js
@@ -631,6 +631,51 @@ objeto a variables con diferentes nombres:
 
 const { johnDoe: { age: userAge, email: userEmail }} = user;
 
+-------------------------------------------------------------------
+
+ejercicio
+
+reemplaza las dos asignaciones con una sintaxis de desestructuración
+equivalente.
+todavía deben seguir asignando las varibales lowToday y highToday
+con los valores de today.low y today.high del objeto LOCAL_FORECAST.
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Cambia solo el código debajo de esta línea
+
+const lowToday = LOCAL_FORECAST.today.low;
+const highToday = LOCAL_FORECAST.today.high;
+
+// Cambia solo el código encima de esta línea
+
+solución:
+
+const LOCAL_FORECAST = {
+  yesterday: { 
+    low: 61, 
+    high: 75 
+    },
+  today: { 
+    low: 64, 
+    high: 77 
+    },
+  tomorrow: {
+     low: 68, 
+     high: 80 
+    }
+};
+
+// Cambia solo el código debajo de esta línea
+
+const { today: { low: lowToday, high: highToday }} = LOCAL_FORECAST;
+
+// Cambia solo el código encima de esta línea
+
 */
 /* */
 /* */
