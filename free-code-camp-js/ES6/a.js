4f68391cc9612e8bd2cb5546e11a1b27f3b53c1f
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-04 18:36:21 -0400

    class 10 fcc-js ES6

--- free-code-camp-js/ES6/a.js
+++ free-code-camp-js/ES6/a.js
@@ -552,7 +552,26 @@ const { today, tomorrow} = HIGH_TEMPERATURES;
 // Cambia solo el código encima de esta línea
 
 */
-/* */
+/* class 10 usa sintaxis de desestructuración para asignar variables desde objetos
+
+la desestructuración te permite asignar un nuevo nombre de variable
+al extraer valores.
+puedes hacer esto al poner el nuevo nombre después de dos puntos al 
+asignar el valor.
+
+esando el mismo objeto del ejemplo anterior:
+
+const user = { name: 'John Doe', age: 34 };
+
+así es como puedes dar nuevos nombres de variables en la asignación;
+
+const { name: userName, age: userAge } = user;
+
+puedes leerlo como "obtén el valor de user.name y asígnalo a una nueva
+variable llamada userName" y así sucesivamente. el valor de userName
+sería la cadena John Doe, y el valor de userAge sería el número 34.
+
+*/
 /* */
 /* */
 /* */
