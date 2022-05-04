6ea001a961d58da4abb2ab1281482b1a89db6cbf
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-04 17:00:02 -0400

    class 8 fcc-js ES6 exercise

--- free-code-camp-js/ES6/a.js
+++ free-code-camp-js/ES6/a.js
@@ -467,6 +467,27 @@ como en un argumento de función o en un arreglo literal.
 el siguiente código no funcionará:
 
 const spreaded = ...arr;
+---------------------------------------------------------
+ejercicio:
+
+copia todo el contenido de arr1 en otro arreglo arr2 usando el 
+operador de propagación.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [];  // Cambia esta línea
+
+console.log(arr2);
+
+solución;
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Cambia esta línea
+
+console.log(arr2);
 
 */
 /* */
