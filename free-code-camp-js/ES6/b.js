e9f71f6889c618dc20746db045c5da85b59fb9b3
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-05 13:12:43 -0400

    class 19 fcc-js ES6

---
+++ free-code-camp-js/ES6/b.js
@@ -0,0 +1,47 @@
+/* class 19 usa sintaxis de clases para definir una función constructora
+
+ES6 proporciona una nueva sintaxis para crear objetos, usando la 
+palabra clave class.
+debemos notar que la sintaxis class es sólo sintaxis, y no una 
+implementación completa basada en clases de un paradigma orientado
+a objetos, a diferencia de lenguajes como java, python, ruby, etc.
+
+en ES5, normalmente definimos una función constructor y usamos la 
+palabra clace new para instanciar un objeto.
+
+var SpaceShuttle = function(targetPlanet){
+  this.targetPlanet = targetPlanet;
+}
+var zeus = new SpaceShuttle('Jupiter');
+
+la sintaxis class simplemente reemplaza la creación de la función
+constructor:
+
+class SpaceShuttle {
+  constructor(targetPlanet) {
+    this.targetPlanet = targetPlanet;
+  }
+}
+const zeus = new SpaceShuttle('Jupiter');
+
+debemos tener en cuenta que la palabra clave class declara una nueva
+función, a la cual se añade un constructor.
+este construictor se invoca cuando new es llamado para crear un nuevo
+objeto.
+
+nota---- UpperCamelCase debe ser utilizado por convención para 
+nombres de clase en ES6.
+el método constructor es un método espacial para crear e inicializar
+un objeto creado con una clase.  
+
+*/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
