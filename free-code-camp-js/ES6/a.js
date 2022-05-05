bd34b0c96bf3781e14fa7f99a7ec5969449644a4
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-04 22:25:36 -0400

    class 16 fcc-js ES6 exercise

--- free-code-camp-js/ES6/a.js
+++ free-code-camp-js/ES6/a.js
@@ -864,6 +864,53 @@ una plantilla de cadena y envolverla con ${y}. del mismo modo, puedes
 incluir otras expresiones en tu literal de cadena, por ejemplo ${a + b}.
 esta nueva forma de crear cadenas te da mayor flexibilidad para crear
 cadenas robustas.
+-----------------------------------------------------------------------
+ejercicio;
+
+Usa la sintaxis de plantilla literal con comillas invertidas para crear un arreglo de cadenas de elemento lista (li). El texto de cada elemento de lista debe ser uno de los elementos del arreglo de la propiedad failure en el objeto result y tener un atributo class con el valor text-warning. La función makeList debe devolver el arreglo de cadenas de elemento lista.
+
+Utiliza un método de iteración (cualquier tipo de bucle) para obtener el resultado deseado (mostrado a continuación).
+
+[
+  '<li class="text-warning">no-var</li>',
+  '<li class="text-warning">var-on-top</li>',
+  '<li class="text-warning">linebreak</li>'
+]
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Cambia solo el código debajo de esta línea
+  const failureItems = [];
+  // Cambia solo el código encima de esta línea
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+solución:
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Cambia solo el código debajo de esta línea
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  // Cambia solo el código encima de esta línea
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
 
 */
 /* */
