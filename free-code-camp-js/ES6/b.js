16f9a8b33de813eb66c921b90723ba1b7aa6f9e0
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-05 13:22:58 -0400

    class 20 fcc-js ES6

--- free-code-camp-js/ES6/b.js
+++ free-code-camp-js/ES6/b.js
@@ -63,7 +63,47 @@ const carrot = new Vegetable('carrot');
 console.log(carrot.name); // Debería mostrar 'carrot'
 
 */
-/* */
+/* class 20 utiliza getters (accesores) y setters (mutadores) para controlar el acceso a un objeto
+
+puedes obtener valores de un objeto y establecer el valor de una
+propiedad dentro de un objeto.
+estas operaciones clásicamente se llaman getters y setters.
+
+las funciones getter están destinadas a simplemente devolver (get)
+el valor de la variable privada de un objeto al usuario sin que el 
+usuario acceda directamente a la variable privada.
+
+las funciones setter están destinadas a modificar (set) el valor de 
+la variable privada de un objeto basado en el valor pasado a la 
+función setter.
+este cambio podría implicar cálculos, o incluso sobrescribir 
+completamente el valor anterir.
+
+class Book {
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer() {
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor) {
+    this._author = updatedAuthor;
+  }
+}
+const novel = new Book('anonymous');
+console.log(novel.writer);
+novel.writer = 'newAuthor';
+console.log(novel.writer);
+
+La consola mostrará las cadenas anonymous y newAuthor.
+
+Ten en cuenta la sintaxis usada para invocar el getter y el setter. Ni siquiera se ven como funciones. Los getter y los setter son importantes porque ocultan los detalles internos de la implementación.
+
+Nota: Es convención preceder el nombre de una variable privada con un guión bajo (_). Sin embargo, la práctica en sí misma no hace una variable privada.
+
+*/
 /* */
 /* */
 /* */
