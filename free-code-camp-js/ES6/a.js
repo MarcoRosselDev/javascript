03aef1c07e3d5367503f11db04886ad6a7eb1c27
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-04 20:11:29 -0400

    class 14 fcc-js ES6 exercise

--- free-code-camp-js/ES6/a.js
+++ free-code-camp-js/ES6/a.js
@@ -743,6 +743,33 @@ el elemento rest sólo funciona correctamente como la última variable
 de la lista. por ejemplo, no puedes usar el parámetro rest para 
 capturar un sub-arreglo que deje fuera el último elemento del erreglo
 original.
+-----------------------------------------------------------------
+ejercicio;
+
+utiliza la sintaxis de desestructuración con el parámetro rest para
+realizar un Array.prototype.slice() eficaz, de tal manera que arr sea
+un sub-arreglo del arreglo original source con los dos primeros 
+elementos omitidos.
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  // Cambia solo el código debajo de esta línea
+  const arr = list; // Cambia esta línea
+  // Cambia solo el código encima de esta línea
+  return arr;
+}
+const arr = removeFirstTwo(source);
+
+solución;
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  // Cambia solo el código debajo de esta línea
+  const [a, b, ...arr] = list; // Cambia esta línea
+  // Cambia solo el código encima de esta línea
+  return arr;
+}
+const arr = removeFirstTwo(source);
 
 */
 /* */
