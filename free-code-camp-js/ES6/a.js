e5a609a105a6a9440a4fb56124b507fb8db665aa
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-04 22:10:38 -0400

    class 15 fcc-js ES6 exercise

--- free-code-camp-js/ES6/a.js
+++ free-code-camp-js/ES6/a.js
@@ -791,6 +791,40 @@ const profileUpdate = ({ name, age, nationality, location }) => {
 cuando profileData es pasado a la función anterior, los valores son
 desestructurados desde el parámetro de función para su uso dentro
 de la función.
+---------------------------------------------------------------
+ejercicio
+
+utiliza la sintaxis de desestructuración dentro del argumento de la 
+función half para enviar solo max y min dentro de la función.
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Cambia solo el código debajo de esta línea
+const half = (stats) => (stats.max + stats.min) / 2.0; 
+// Cambia solo el código encima de esta línea
+
+solución:
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Cambia solo el código debajo de esta línea
+// const half = (stats) => (stats.max + stats.min) / 2.0; 
+const half = ({max, min}) => (max + min) / 2.0;
+// Cambia solo el código encima de esta línea
 
 */
 /* */
