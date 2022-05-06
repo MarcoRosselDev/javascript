f24786b44131c871f3e204b28d5cc8e27929b479
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-05 22:10:30 -0400

    class 24 fcc-js ES6

--- free-code-camp-js/ES6/b.js
+++ free-code-camp-js/ES6/b.js
@@ -299,7 +299,18 @@ uppercaseString("hello");
 lowercaseString("WORLD!");
 
 */
-/* */
+/* class 24 use * para importar todo de un archivo
+
+Supongamos que tienes un archivo y deseas importar todo su contenido en el archivo actual. Esto puede hacerse con la sintaxis import * as. Este es un ejemplo donde los contenidos de un archivo llamado math_functions.js son importados a un archivo dentro del mismo directorio:
+
+import * as myMathModule from "./math_functions.js";
+
+La anterior declaración import, crea un objeto llamado myMathModule. Esto es, sólo el nombre de una variable, puedes nombrarlo de cualquier manera. El objeto contiene todas las exportaciones de math_functions.js, así puedes acceder a las funciones, como haces con cualquier propiedad del objeto. A continuación puedes usar las funciones importadas add y subtract:
+
+myMathModule.add(2,3);
+myMathModule.subtract(5,3);
+
+*/
 /* */
 /* */
 /* */
