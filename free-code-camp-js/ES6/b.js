032b6d66b81409d0b2d42f904b2ecad8c302ee35
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-05 21:09:11 -0400

    class 22 fcc-js ES6 exercise

--- free-code-camp-js/ES6/b.js
+++ free-code-camp-js/ES6/b.js
@@ -228,6 +228,32 @@ por ejemplo:
 
 export { add, subtract };
 
+---------------------------------------------------------------
+ejercicio:
+
+hay dos funciones relacionadas con cadenas en el editor.
+exporta ambos utilizando el método que elijas.
+
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+solución:
+
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export { uppercaseString, lowercaseString};
+
 */
 /* */
 /* */
