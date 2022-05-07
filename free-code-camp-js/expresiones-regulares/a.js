b08248e65518dbe863107cc345cd298f96ff160e
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-07 18:37:25 -0400

    class 9 fcc-js expresiones-regulares

--- free-code-camp-js/expresiones-regulares/a.js
+++ free-code-camp-js/expresiones-regulares/a.js
@@ -319,7 +319,32 @@ let vowelRegex = /[aeiou]/gi; // Cambia esta línea
 let result = quoteSample.match(vowelRegex); // Cambia esta línea
 
 */
-/* */
+/* class 9 haz coincidir las letras del alfabeto
+
+Has visto cómo puedes usar los conjuntos de caracteres para especificar 
+un grupo de caracteres a coincidir, pero eso requiere escribir mucho 
+cuando necesitas coincidir con un amplio rango de caracteres (por 
+ejemplo, cada letra en el alfabeto). Afortunadamente, hay una 
+funcionalidad incorporada que hace esto corto y sencillo.
+
+Dentro de un conjunto de caracteres, puedes definir un rango de 
+caracteres a coincidir usando un carácter de guion: -.
+
+Por ejemplo, para hacer coincidir las letras minúsculas desde la a a la e 
+usarías [a-e].
+
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let bgRegex = /[a-e]at/;
+catStr.match(bgRegex);
+batStr.match(bgRegex);
+matStr.match(bgRegex);
+
+En orden, las tres llamadas a match devolverán los valores ["cat"], 
+["bat"] y null.
+
+*/
 /* */
 /* */
 /* */
