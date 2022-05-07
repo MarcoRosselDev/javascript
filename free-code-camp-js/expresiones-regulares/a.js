d89ffa78ed05cebf1bf3d292a12c5ed6607c88b2
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-07 19:27:26 -0400

    class 10 fcc-js expresiones-regulares exercise

--- free-code-camp-js/expresiones-regulares/a.js
+++ free-code-camp-js/expresiones-regulares/a.js
@@ -379,6 +379,24 @@ let jennyStr = "Jenny8675309";
 let myRegex = /[a-z0-9]/ig;
 jennyStr.match(myRegex);
 
+------------------------------------------------------------------
+ejercicio;
+
+crea una sola expresión regular que coincida con un rango de letras 
+entre h y s, y un rango de números entre 2 y 6.
+recuerda incluir las banderas apropiadas en la expresión regular.
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /change/; // Cambia esta línea
+let result = myRegex; // Cambia esta línea
+
+
+solución;
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Cambia esta línea
+let result = quoteSample.match(myRegex); // Cambia esta línea
+
 */
 /* */
 /* */
