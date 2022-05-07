ec2abf6cebb80d8223bb1a4c5288dd1e0e369c78
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-07 13:08:35 -0400

    class 7 fcc-js expresiones-regulares

--- free-code-camp-js/expresiones-regulares/a.js
+++ free-code-camp-js/expresiones-regulares/a.js
@@ -228,7 +228,27 @@ let result = twinkleStar.match(starRegex); // Cambia esta línea
 
 
 */
-/* */
+/* class 7 haz coincidir cualquier cosa con el comodín punto
+
+A veces no conoces (o no necesitas conocer) los caracteres exactos en tus 
+patrones. Pensar en todas las palabras que coincidan, digamos, con una 
+ortografía errónea llevaría mucho tiempo. Afortunadamente, puedes ahorrar 
+tiempo utilizando el carácter de comodín: .
+
+El carácter de comodín . coincidirá con cualquier carácter único. El 
+comodín también es llamado dot y period. Puedes utilizar el carácter de 
+comodín como cualquier otro carácter en la expresión regular. Por 
+ejemplo, si quieres hacer coincidir hug, huh, hut, y hum, puedes usar la 
+la expresión regular /hu./ para que coincida con las cuatro palabras.
+
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+huRegex.test(humStr);
+huRegex.test(hugStr);
+Ambas llamadas a test devolverán true.
+
+*/
 /* */
 /* */
 /* */
