faf8a8df89684d876f61e2363e27cae0efc3ea37
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-07 12:41:05 -0400

    class 3 fcc-js expresiones-regulares

--- free-code-camp-js/expresiones-regulares/a.js
+++ free-code-camp-js/expresiones-regulares/a.js
@@ -63,7 +63,17 @@ let waldoRegex = /Waldo/; // Cambia esta línea
 let result = waldoRegex.test(waldoIsHiding);
 
 */
-/* */
+/* class 3 coincide una cadena literal con diferentes posibilidades
+
+Al utilizar expresiones regulares como /coding/, puedes buscar el patrón coding en otra cadena.
+
+Esto es muy potente al buscar cadenas individuales, pero está limitado a un solo patrón. Puedes buscar múltiples patrones usando el operador alternation o OR: |.
+
+Este operador coincide con los patrones antes o después de él. Por ejemplo, si deseas coincidir con las cadenas yes o no, la expresión regular que quieres es /yes|no/.
+
+También puedes buscar más de dos patrones. Puedes hacer esto añadiendo más patrones con más operadores OR separándolos, como /yes|no|maybe/.
+
+*/
 /* */
 /* */
 /* */
