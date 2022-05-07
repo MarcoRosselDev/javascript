ada2d2bdf913a5466b4a49d00b428273994d711e
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-07 13:16:19 -0400

    class 8 fcc-js expresiones-regulares

--- free-code-camp-js/expresiones-regulares/a.js
+++ free-code-camp-js/expresiones-regulares/a.js
@@ -268,7 +268,36 @@ let unRegex = /.un/; // Cambia esta línea
 let result = unRegex.test(exampleStr);
 
 */
-/* */
+/* class 8 haz coincidir un solo carácter con múltiples posibilidades
+
+Aprendiste cómo hacer coincidir los patrones literales (/literal/) y el 
+carácter de comodín (/./). Estos son los extremos de las expresiones 
+regulares, donde uno encuentra coincidencias exactas y el otro coincide 
+de todo. Hay opciones que representan un equilibrio entre los dos 
+extremos.
+
+Puedes buscar un patrón literal con cierta flexibilidad utilizando las 
+clases de caracteres. Las clases de caracteres te permiten definir un 
+grupo de caracteres que deseas coincidir colocándolos dentro de corchetes 
+([ y ]).
+
+Por ejemplo, si quieres hacer coincidir bag, big, y bug pero no bog. 
+Puedes crear la expresión regular /b[aiu]g/ para hacer esto. [aiu] es la 
+clase de caracteres que solo coincidirá con los caracteres a, i, o u.
+
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+bigStr.match(bgRegex);
+bagStr.match(bgRegex);
+bugStr.match(bgRegex);
+bogStr.match(bgRegex);
+En orden, las cuatro llamadas de match devolverán los valores ["big"], 
+["bag"], ["bug"], and null.
+
+*/
 /* */
 /* */
 /* */
