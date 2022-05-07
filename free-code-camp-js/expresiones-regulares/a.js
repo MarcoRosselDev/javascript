7f9c3613fc7bedadc7dc03bdb14073afae7c3849
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-07 19:29:08 -0400

    class 11 fcc-js expresiones-regulares

--- free-code-camp-js/expresiones-regulares/a.js
+++ free-code-camp-js/expresiones-regulares/a.js
@@ -398,7 +398,23 @@ let myRegex = /[h-s2-6]/gi; // Cambia esta línea
 let result = quoteSample.match(myRegex); // Cambia esta línea
 
 */
-/* */
+/* class 11 haz coincidir caracteres individuales no especificados
+
+Hasta ahora, has creado un conjunto de caracteres que deseas coincidir, 
+pero también podrías crear un conjunto de caracteres que no desees 
+coincidir. Este tipo de conjuntos de caracteres se llaman conjuntos de }
+caracteres negados.
+
+Para crear un conjunto de caracteres negados colocas un carácter de 
+intercalación (^) después del corchete de apertura y antes de los 
+caracteres que no quieres coincidir.
+
+Por ejemplo, /[^aeiou]/gi coincide con todos los caracteres que no son 
+una vocal. Ten en cuenta que caracteres como ., !, [, @, / y el espacio 
+en blanco coinciden; el conjunto de caracteres de vocal negados sólo 
+excluye los caracteres de vocal.
+
+*/
 /* */
 /* */
 /* */
