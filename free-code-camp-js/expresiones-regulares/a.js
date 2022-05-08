84395072b8c9f69efc0c2e94366c8c593e749593
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-08 16:49:01 -0400

    class 13 fcc-js expresiones-regulares exercise

--- free-code-camp-js/expresiones-regulares/a.js
+++ free-code-camp-js/expresiones-regulares/a.js
@@ -482,14 +482,38 @@ El carácter que hace esto es el asterisco o la estrella: *.
 let soccerWord = "gooooooooal!";
 let gPhrase = "gut feeling";
 let oPhrase = "over the moon";          
-let goRegex = /go*-/;             (el guion es para que no se rompa el  
-                                    codigo )            
+// let goRegex = /go* /;            
 soccerWord.match(goRegex);
 gPhrase.match(goRegex);
 oPhrase.match(goRegex);
 
 En orden, los tres match devolverán los valores ["goooooooo"], ["g"], y 
 null.
+
+--------------------------------------------------------------------
+ejercicio;
+
+Para este desafío, chewieQuote ha sido inicializada entre bastidores con 
+la cadena Aaaaaaaaaaaaaaaarrrgh!. Crea una expresión regular chewieRegex 
+que utilice el carácter * para encontrar el carácter A mayúscula seguido 
+inmediatamente por cero o más caracteres a minúscula en chewieQuote. Tu }
+expresión regular no necesita banderas o clases de caracteres, y no debe }
+coincidir con ninguna de las otras comillas.
+
+// Cambia solo el código debajo de esta línea
+let chewieRegex = /change/; // Cambia esta línea
+// Cambia solo el código encima de esta línea
+
+let result = chewieQuote.match(chewieRegex);
+
+
+solución:
+
+// Cambia solo el código debajo de esta línea
+// let chewieRegex = /Aa* /; // Cambia esta línea
+// Cambia solo el código encima de esta línea
+
+// let result = chewieQuote.match(chewieRegex);
 
 */
 /* */
