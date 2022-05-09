d3de03a55a2f1b45e8073e764655850152448048
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-09 01:16:16 -0400

    class 23 fcc-js expresiones-regulares

--- free-code-camp-js/expresiones-regulares/b.js
+++ free-code-camp-js/expresiones-regulares/b.js
@@ -173,7 +173,25 @@ let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; // Cambia esta línea
 let result = userCheck.test(username);
 
 */
-/* */
+/* class 23 haz coincidir espacios en blanco
+
+Los desafíos por el momento han cubierto las letras que coinciden con 
+el alfabeto y los números. También puedes hacer que coincidan los 
+espacios en blanco o los espacios entre las letras.
+
+Puedes buscar los espacios en blanco usando \s que es una s 
+minúscula. Este patrón no solo coincide con los espacios en blanco, 
+también con los caracteres de retorno de carro, tabulaciones, 
+alimentación de formulario y saltos de línea. Puedes pensar que es 
+similar a las clases de caracteres [ \r\t\f\n\v].
+
+let whiteSpace = "Whitespace. Whitespace everywhere!"
+let spaceRegex = /\s/g;
+whiteSpace.match(spaceRegex);
+
+Esta llamada a match debe devolver [" ", " "].
+
+*/
 /* */
 /* */
 /* */
