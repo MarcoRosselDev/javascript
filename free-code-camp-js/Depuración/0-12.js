903e8b6d7ed39f5ff298ad114c4d7f9d74f7393b
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-10 18:22:00 -0400

    class 1 fcc-js depuración

---
+++ free-code-camp-js/Depuración/0-12.js
@@ -0,0 +1,37 @@
+DEPUTACION
+
+/*La depuración es el proceso de revisar tu código, encontrando 
+cualquier problema, y arreglándolo.
+
+
+Los problemas en el código generalmente vienen en tres formas: errores 
+de sintaxis que impiden que tu programa se ejecute, errores de tiempo de 
+ejecución donde tu código tiene un comportamiento inesperado, o errores 
+lógicos donde tu código no hace lo que pretendes.
+
+En este curso, aprenderás como usar la consola de JavaScript para 
+depurar programas y prevenir errores comunes antes de que ocurran. */
+
+/* class 1 usa la consola de js para comprobar el valor de una variable
+
+Tanto Chrome como Firefox tienen excelentes consolas de JavaScript, también conocidas como DevTools, para depurar tu JavaScript.
+
+Puedes encontrar las herramientas para desarrolladores en el menú de Chrome o la consola web en el menú de Firefox. Si utilizas otro navegador, o un teléfono móvil, te recomendamos encarecidamente que cambies a Firefox o Chrome de escritorio.
+
+El método console.log(), que "imprime" la salida de lo que está dentro de sus paréntesis a la consola, será probablemente la herramienta de depuración más útil. Colocarlo en puntos estratégicos de tu código puede mostrarte los valores intermedios de las variables. Es una buena práctica tener una idea de lo que debería ser la salida antes de ver lo que es. Tener puntos de control para ver el estado de tus cálculos a lo largo de tu código ayudará a acotar dónde está el problema.
+
+Aquí hay un ejemplo para imprimir la cadena Hello world! en la consola:
+
+console.log('Hello world!');
+
+*/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
