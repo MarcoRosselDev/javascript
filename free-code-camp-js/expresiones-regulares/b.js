3d23c923e72e590b9c60c9d5d5fe50db80681276
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-10 00:19:27 -0400

    class 30 fcc-js expresiones-regulares exercise

--- free-code-camp-js/expresiones-regulares/b.js
+++ free-code-camp-js/expresiones-regulares/b.js
@@ -480,6 +480,32 @@ let testRegex = /P(engu|umpk)in/;
 testRegex.test(testStr);
 El método test aquí devolverá true.
 
+-------------------------------------------------------------------
+ejercicio;
+
+Corrige la expresión regular para que compruebe los nombres de Franklin 
+Roosevelt o Eleanor Roosevelt de una manera sensible a mayúsculas y 
+minúsculas y haciendo concesiones para los segundos nombres.
+
+Luego, corrige el código para que la expresión regular que has creado 
+se compruebe con myString y devuelva true o false dependiendo de si la 
+expresión regular coincide.
+
+let myString = "Eleanor Roosevelt";
+let myRegex = /False/; // Cambia esta línea
+let result = false; // Cambia esta línea
+// Después de pasar el desafío experimenta con myString y observa cómo 
+funciona la agrupación
+
+
+solución;
+
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor).*Roosevelt/; // Cambia esta línea
+let result = myRegex.test(myString); // Cambia esta línea
+// Después de pasar el desafío experimenta con myString y observa cómo 
+funciona la agrupación
+
 */
 /* */
 /* */
