8384cae7e5a051f29a17ca23774b7409c0f92c08
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-09 22:56:46 -0400

    class 27 fcc-js expresiones-regulares

--- free-code-camp-js/expresiones-regulares/b.js
+++ free-code-camp-js/expresiones-regulares/b.js
@@ -325,5 +325,27 @@ let haRegex = /Haz{4,}ah/; // Cambia esta línea
 let result = haRegex.test(haStr);
 
 */
-/* */
+/* class 27 especifica el número exacto de coincidencias
+
+Puedes especificar el número inferior y superior de patrones mediante 
+especificadores de cantidad utilizando llaves. A veces solo quieres un 
+número específico de coincidencias.
+
+Para especificar un cierto número de patrones, simplemente pon ese 
+número entre corchetes.
+
+Por ejemplo, para que coincida con la palabra hah solo con la letra a 3 
+veces, tu expresión regular sera /ha{3}h/.
+
+let A4 = "haaaah";
+let A3 = "haaah";
+let A100 = "h" + "a".repeat(100) + "h";
+let multipleHA = /ha{3}h/;
+multipleHA.test(A4);
+multipleHA.test(A3);
+multipleHA.test(A100);
+
+En orden, las tres llamadas a test devuelven false, true y false.
+
+*/
 /* */
