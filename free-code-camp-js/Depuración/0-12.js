b32efbe4321a577831faba0a7bd1073ffec8e5b3
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-10 22:48:24 -0400

    class 4 fcc-js depuración

--- free-code-camp-js/Depuración/0-12.js
+++ free-code-camp-js/Depuración/0-12.js
@@ -111,7 +111,13 @@ console.log(typeof seven);
 console.log(typeof three);
 
 */
-/* */
+/* class 4 captura nombres de variables y funciones mal escritos 
+
+Los métodos console.log() y typeof son las dos formas principales de comprobar los valores intermedios y los tipos de salida de un programa. Ahora es el momento de entrar en las formas comúnes que adoptan los errores (bugs). Un problema a nivel de sintaxis con el que las personas que escriben rápido pueden simpatizar es el humilde error ortográfico.
+
+Los caracteres transpuestos, omitidos o mal escritos en el nombre de una variable o función harán que el navegador busque un objeto que no existe, y se queje en forma de error de referencia. Los nombres de variables y funciones de JavaScript distinguen entre mayúsculas y minúsculas.
+
+*/
 /* */
 /* */
 /* */
