69c3ce3320fb236cd096466a0f4c08ab72c21bdc
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-11 00:51:49 -0400

    class 9 fcc-js depuración

--- free-code-camp-js/Depuración/0-12.js
+++ free-code-camp-js/Depuración/0-12.js
@@ -292,6 +292,10 @@ let result = getNine();
 console.log(result);
 
 */
-/* */
+/* class 9 captura argumentos pasados en el orden incorrecto al llamar a una función
+
+Siguiendo con la discusión sobre la llamada a funciones, el siguiente error a tener en cuenta es cuando los argumentos de una función se suministran en el orden incorrecto. Si los argumentos son de tipos diferentes, como una función que espera un arreglo y un entero, es probable que se produzca un error de ejecución. Si los argumentos son del mismo tipo (todos enteros, por ejemplo), la lógica del código no tendrá sentido. Asegúrate de proporcionar todos los argumentos requeridos, en el orden correcto para evitar estos problemas.
+
+*/
 /* */
 /* */
