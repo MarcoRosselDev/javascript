b8751de2482a292511bbac340d4599785e349018
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-10 22:53:50 -0400

    class 5 fcc-js depuración

--- free-code-camp-js/Depuración/0-12.js
+++ free-code-camp-js/Depuración/0-12.js
@@ -137,7 +137,13 @@ let netWorkingCapital = receivables - payables;
 console.log(`Net working capital is: ${netWorkingCapital}`);
 
 */
-/* */
+/* class 5 captura paréntesis, corchetes, llaves y comillas sin cerrar
+
+Otro error de sintaxis a tener en cuenta es que todos los paréntesis de apertura, corchetes, llaves y comillas tienen un par de cierre. Olvidar una pieza suele suceder cuando se edita el código existente y se insertan elementos con uno de los tipos de pares. También hay que tener cuidado al anidar bloques de código dentro de otros, como agregar una función de callback como argumento de un método.
+
+Una forma de evitar este error es, tan pronto como se escriba el caracter de apertura, incluir inmediatamente su caracter de cierre, luego mover el cursor hacia atrás entre ellos y continuar escribiendo. Afortunadamente, la mayoría de los editores de código modernos generan la segunda mitad del par automáticamente.
+
+*/
 /* */
 /* */
 /* */
