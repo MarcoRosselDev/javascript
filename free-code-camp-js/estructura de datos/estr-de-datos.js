1b35bdd3318860c96e2c63dc11e8966cca6300ef
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-12 03:00:48 -0400

    class 5 fcc-js est-datos

--- free-code-camp-js/estructura de datos/estr-de-datos.js
+++ free-code-camp-js/estructura de datos/estr-de-datos.js
@@ -188,7 +188,27 @@ function popShift(arr) {
 console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
 */
-/* */
+/* class 5 elimina elementos usando splice()
+
+Bien, ya hemos aprendido a eliminar elementos al principio y al final de los arreglos utilizando shift() y pop(), pero ¿qué pasa si queremos eliminar un elemento de alguna parte del medio? ¿O eliminar más de un elemento a la vez? Pues bien, ahí es donde entra splice(). splice() nos permite hacer precisamente eso: eliminar cualquier número de elementos consecutivos de cualquier parte de un arreglo.
+
+splice() puede tomar hasta 3 parámetros, pero por ahora, nos centraremos sólo en los 2 primeros. Los primeros dos parámetros de splice() son enteros que representan índices, o posiciones, de elementos en el arreglo a la que splice() está siendo llamado. Y recuerda que los arreglos están indexados en cero, por lo que para indicar el primer elemento de un arreglo, usaríamos 0. El primer parámetro de splice() representa el índice del arreglo a partir del cual se empiezan a eliminar los elementos, mientras que el segundo parámetro indica el número de elementos a eliminar. Por ejemplo:
+
+let array = ['today', 'was', 'not', 'so', 'great'];
+
+array.splice(2, 2);
+
+Aquí eliminamos 2 elementos, comenzando con el tercer elemento (en el índice 2). array tendrá el valor ['today', 'was', 'great'].
+
+splice() no sólo modifica el arreglo que llama, sino que también devuelve un nuevo arreglo que contiene el valor de los elementos eliminados:
+
+let array = ['I', 'am', 'feeling', 'really', 'happy'];
+
+let newArray = array.splice(3, 2);
+
+newArray tiene el valor ['really', 'happy'].
+
+*/
 /* */
 /* */
 /* */
