24ace090b6005119c6ad8c49684d4f36bbc50e35
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-11 20:02:51 -0400

    class 12} fcc-js depuración exercise

--- free-code-camp-js/Depuración/0-12.js
+++ free-code-camp-js/Depuración/0-12.js
@@ -448,5 +448,26 @@ function loopy() {
 
 El trabajo del programador es asegurarse de que la condición terminal, que indica al programa cuándo debe salir del código del bucle, se alcance finalmente. Un error es incrementar o decrementar una variable de contador en la dirección incorrecta desde la condición terminal. Otra es reiniciar accidentalmente un contador o una variable de índice dentro del código del bucle, en lugar de incrementarlo o decrementarlo.
 
+------------------------------------------------------------------------
+ejercicio;
+
+La función myFunc() contiene un bucle infinito porque la condición terminal i != 4 nunca se evaluará a false (y romperá el bucle) - i se incrementará en 2 en cada pasada, y saltará justo sobre 4, ya que i es impar para empezar. Corrige el operador de comparación en la condición terminal para que el bucle sólo se ejecute para i menor o igual a 4.
+
+function myFunc() {
+  for (let i = 1; i != 4; i += 2) {
+    console.log("Still going!");
+  }
+}
+
+
+
+solución;
+
+function myFunc() {
+  for (let i = 1; i <= 4; i += 2) {
+    console.log("Still going!");
+  }
+}
+
 */
 /* */
