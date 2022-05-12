9066dc223d8259ccb00290faf1a49f6d2e0a0fbc
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-12 01:27:35 -0400

    class 2 fcc-js est-datos

--- free-code-camp-js/estructura de datos/estr-de-datos.js
+++ free-code-camp-js/estructura de datos/estr-de-datos.js
@@ -51,7 +51,27 @@ let yourArray = [
 ]; // Cambia esta línea
 
 */
-/* */
+/* class 2 accede a los contenidos de u arreglo utilizando la notación de corchetes
+
+La principal característica de cualquier estructura de datos es, por supuesto, la habilidad no solo de guardar datos, sino también de ser capaz de recuperar esos datos cuando le es requerido. Entonces, ahora que hemos aprendido como crear un arreglo, comencemos a pensar en cómo podemos acceder a la información de ese arreglo.
+
+Cuando definimos un arreglo simple como el que se ve a continuación, hay 3 elementos en él:
+
+let ourArray = ["a", "b", "c"];
+
+En un arreglo, cada elemento tiene un índice. Este índice funciona como la posición de ese elemento en el arreglo y es como puedes referenciarlo. Sin embargo, es importante tener en cuenta, que los arreglos en JavaScript son indexados en base cero, es decir que el primer elemento de un arreglo está en la posición cero, no en la uno. Para recuperar un elemento de un arreglo podemos encerrar un índice entre corchetes y agregarlo al final de este, o más comúnmente, a una variable que hace referencia a un objeto tipo arreglo. Esto es conocido como notación de corchetes. Por ejemplo, si queremos recuperar la a de ourArray y asignársela a una variable, podemos hacerlo con el siguiente código:
+
+let ourVariable = ourArray[0];
+
+Ahora ourVariable tiene el valor de a.
+
+Además de acceder al valor asociado con un índice, también puedes establecer un índice a un valor usando la misma notación:
+
+ourArray[1] = "not b anymore";
+
+Utilizando la notación de corchetes, ahora hemos restablecido el elemento en el índice 1 de la cadena b, a not b anymore. Ahora ourArray es ["a", "not b anymore", "c"].
+
+*/
 /* */
 /* */
 /* */
