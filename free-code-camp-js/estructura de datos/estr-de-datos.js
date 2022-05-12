064c8ef07d2b77f97f339f65194db0dc5568c575
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-11 20:14:51 -0400

    class 1 fcc-js est-datos exercise

--- free-code-camp-js/estructura de datos/estr-de-datos.js
+++ free-code-camp-js/estructura de datos/estr-de-datos.js
@@ -4,6 +4,7 @@ Lo siguiente es un ejemplo de la implementación más simple de una estructura d
 
 let simpleArray = ['one', 2, 'three', true, false, undefined, null];
 console.log(simpleArray.length);
+
 La llamada console.log muestra 7.
 
 Todos los arreglos tienen una propiedad de longitud, que como se muestra arriba, se puede acceder muy fácilmente con la sintaxis Array.length. A continuación se puede ver una implementación más compleja de un arreglo. Esto se conoce como un arreglo multidimensional, o un arreglo que contiene otros arreglos. Observa que este arreglo también contiene objetos JavaScript, que examinaremos muy de cerca en la siguiente sección, pero por ahora, todo lo que necesitas saber es que los arreglos también son capaces de almacenar objetos complejos.
@@ -30,6 +31,24 @@ let complexArray = [
     }
   ]
 ];
+
+---------------------------------------------------------------
+ejercicio;
+
+Hemos definido una variable llamada yourArray. Completa la sentencia asignando un arreglo de al menos 5 elementos de longitud a la variable yourArray. Tu arreglo debe contener al menos una cadena (string), un número (number) y un booleano (boolean).
+
+let yourArray; // Cambia esta línea
+
+
+solución;
+
+let yourArray = [
+  "moko",
+  12,
+  true,
+  "loco",
+  9
+]; // Cambia esta línea
 
 */
 /* */
