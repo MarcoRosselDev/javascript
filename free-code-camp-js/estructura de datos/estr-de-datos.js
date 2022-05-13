09acce32790316aea6423a4a573d46e7fa8a2236
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-13 05:43:57 -0400

    class 17 fcc-js est-datos

--- free-code-camp-js/estructura de datos/estr-de-datos.js
+++ free-code-camp-js/estructura de datos/estr-de-datos.js
@@ -799,7 +799,83 @@ delete foods.strawberries;
 console.log(foods);
 
 */
-/* */
+/* class 17 evalúa si un objeto tiene una propiedad
+
+Ahora podemos agregar, modificar y eliminar claves de los objetos. Pero, ¿y si sólo queremos saber si un objeto tiene una propiedad específica? JavaScript nos proporciona dos maneras diferentes de hacerlo. Uno utiliza el método hasOwnProperty() y el otro utiliza la palabra clave in. Si tenemos un objeto users con una propiedad de Alan, podríamos comprobar su presencia de cualquiera de las siguientes maneras:
+
+users.hasOwnProperty('Alan');
+'Alan' in users;
+Ambos devuelven true.
+
+--------------------------------------------------------------------
+ejercicio:
+
+Termina de escribir la función para que devuelva true si el objeto pasado contiene los cuatro nombres, Alan, Jeff, Sarah and Ryan y devuelva false en caso contrario.
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(userObj) {
+  // Cambia solo el código debajo de esta línea
+
+  // Cambia solo el código encima de esta línea
+}
+
+console.log(isEveryoneHere(users));
+
+
+solución;
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(userObj) {
+  // Cambia solo el código debajo de esta línea
+  if (userObj.hasOwnProperty('Alan')&&userObj.hasOwnProperty('Sarah')&&userObj.hasOwnProperty('Jeff')&&userObj.hasOwnProperty('Ryan')) {
+    return true;
+  }
+  else{
+    return false;
+  }
+  // Cambia solo el código encima de esta línea
+  
+}
+
+console.log(isEveryoneHere(users));
+
+*/
 /* */
 /* */
 /* */
