0e677b3685fccec47fdb4d53781e34a562127c00
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-12 22:04:31 -0400

    class 10 fcc-js est-datos

--- free-code-camp-js/estructura de datos/estr-de-datos.js
+++ free-code-camp-js/estructura de datos/estr-de-datos.js
@@ -396,7 +396,22 @@ function spreadOut() {
 console.log(spreadOut());
 
 */
-/* */
+/* class 10 comprueba la presencia de un elemento con indexOf()
+
+Ya que los arreglos pueden modificarse, o mutarse, en cualquier momento, no se puede garantizar dónde estará un dato concreto en un arreglo determinado, o si ese elemento sigue existiendo. Afortunadamente, JavaScript nos proporciona otro método incorporado, indexOf(), que nos permite comprobar rápida y fácilmente la presencia de un elemento en un arreglo. indexOf() toma un elemento como parámetro, y cuando lo llama, devuelve la posición, o índice, de ese elemento, o -1 si el elemento no existe en el arreglo.
+
+Por ejemplo:
+
+let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+
+fruits.indexOf('dates');
+fruits.indexOf('oranges');
+fruits.indexOf('pears');
+
+indexOf('dates') devuelve -1, indexOf('oranges') devuelve 2, e 
+indexOf('pears') devuelve 1 (el primer índice en el que existe cada elemento).
+
+*/
 /* */
 /* */
 /* */
