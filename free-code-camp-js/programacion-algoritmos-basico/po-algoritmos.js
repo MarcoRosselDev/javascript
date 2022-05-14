6dd0e0177d7a4744f798cc62fe7b4a7b3202db7c
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-14 19:40:01 -0400

    class 4 fcc-js algoritmos exercise

--- free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
+++ free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
@@ -100,7 +100,23 @@ findLongestWordLength("The quick brown fox jumped over the lazy dog");
 
 solución;
 
+function findLongestWordLength(str) {
 
+  let words = str.split(' ');
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+  
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+
+  }
+
+  return maxLength;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
 
 */
 /* */
