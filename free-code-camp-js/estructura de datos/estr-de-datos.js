b8a6382cbbcfc9fe13d3b5c362203af0515d5983
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-13 22:32:23 -0400

    class 19 fcc-js est-datos exercise

--- free-code-camp-js/estructura de datos/estr-de-datos.js
+++ free-code-camp-js/estructura de datos/estr-de-datos.js
@@ -978,7 +978,32 @@ console.log(getArrayOfUsers(users));
 
 solución;
 
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
 
+function getArrayOfUsers(obj) {
+  // Cambia solo el código debajo de esta línea
+  return Object.keys(obj);
+  // Cambia solo el código encima de esta línea
+}
+
+console.log(getArrayOfUsers(users));
 
 */
 /* */
