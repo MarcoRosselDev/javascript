25d8b1a370580a7c7c4a54dfb1840fed11e83ec5
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-13 22:44:28 -0400

    class 1 fcc-js algoritmos

---
+++ free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
@@ -0,0 +1,29 @@
+/* class 1 convierte celsius a fahrenhait
+
+La fórmula para convertir de Celsius a Fahrenheit es la temperatura en Celsius multiplicado por 9/5, más 32.
+
+Se te da una variable celsius que representa una temperatura en Celsius. Utiliza la variable fahrenheit ya definida y asígnale la temperatura Fahrenheit equivalente a la temperatura Celsius dada. Utiliza la fórmula mencionada anteriormente para ayudarte a convertir la temperatura Celsius a Fahrenheit.
+
+function convertToF(celsius) {
+  let fahrenheit;
+  return fahrenheit;
+}
+
+convertToF(30);
+
+*/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
