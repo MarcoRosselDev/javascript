cac24f84d181512fb4862f3aab945a2767e364ed
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-13 22:23:04 -0400

    class 19 fcc-js est-datos

--- free-code-camp-js/estructura de datos/estr-de-datos.js
+++ free-code-camp-js/estructura de datos/estr-de-datos.js
@@ -938,5 +938,47 @@ function countOnline(usersObj) {
 console.log(countOnline(users));
 
 */
-/* */
+/* class 19 genera un arreglo de todas las claves de los objetos con Object.keus()
+
+También podemos generar un arreglo que contenga todas las claves almacenadas en un objeto utilizando el método Object.keys() y pasando un objeto como argumento. Esto devolverá un arreglo con cadenas que representan cada propiedad del objeto. De nuevo, no habrá un orden específico para las entradas en el arreglo.
+
+-----------------------------------------------------------------------
+ejercicio;
+
+Termina de escribir la función getArrayOfUsers para que devuelva un arreglo que contenga todas las propiedades del objeto que recibe como argumento.
+
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // Cambia solo el código debajo de esta línea
+
+  // Cambia solo el código encima de esta línea
+}
+
+console.log(getArrayOfUsers(users));
+
+
+
+solución;
+
+
+
+*/
 /* */
