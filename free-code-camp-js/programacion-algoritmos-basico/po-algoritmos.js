f5d27850a71c64ba86e42c38f012d2c172234dad
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-14 18:54:13 -0400

    class 3 fcc-js algoritmos

--- free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
+++ free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
@@ -52,7 +52,39 @@ function reverseString(str) {
 reverseString("hello");
 
 */
-/* */
+/* class 3 factoriza un número
+
+Devuelve el factorial del entero proporcionado.
+
+Si el número entero es representado con la letra n, un factorial es el producto de todos los enteros positivos menores o iguales a n.
+
+Los factoriales suelen representarse con la abreviatura n!
+
+Por ejemplo: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Sólo se proporcionarán a la función los enteros mayores o iguales a cero.
+
+
+function factorialize(num) {
+  return num;
+}
+
+factorialize(5);
+
+---------------------------------------------------------------
+solución;
+
+function factorialize(num) {
+  let product = 1;
+  for (let i = 2; i <= num; i++) {
+    product *= i;
+  }
+  return product;
+}
+
+factorialize(5);
+
+*/
 /* */
 /* */
 /* */
