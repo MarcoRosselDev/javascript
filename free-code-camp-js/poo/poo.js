9413c5f1c9e9251cef4100509cf7c08e9a0c76b8
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-15 16:33:15 -0400

    class 2 fcc-js poo

--- free-code-camp-js/poo/poo.js
+++ free-code-camp-js/poo/poo.js
@@ -33,7 +33,20 @@ let dog = {
 };
 
 */
-/* */
+/* class 2 utiliza notación de puntos para acceder a las propiedades de un objeto
+
+En el último desafío creaste un objeto con varias propiedades. Ahora verás cómo acceder a los valores de esas propiedades. Por ejemplo:
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2
+};
+console.log(duck.name);
+Se utiliza notación de puntos con el nombre del objeto, duck, seguido por el nombre de la propiedad, name, para acceder al valor de Aflac.
+
+
+
+*/
 /* */
 /* */
 /* */
