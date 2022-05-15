b0b4d58543c83b344feb1eaa2fc01287e30305ed
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-15 16:37:31 -0400

    class 2 fcc-js poo exercise

--- free-code-camp-js/poo/poo.js
+++ free-code-camp-js/poo/poo.js
@@ -44,7 +44,28 @@ let duck = {
 console.log(duck.name);
 Se utiliza notación de puntos con el nombre del objeto, duck, seguido por el nombre de la propiedad, name, para acceder al valor de Aflac.
 
+--------------------------------------------------------------------
+ejercicio;
 
+Imprime ambas propiedades del objeto dog en tu consola.
+
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+// Cambia solo el código debajo de esta línea
+
+--------------------------------------------------------------------
+solución;
+
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+// Cambia solo el código debajo de esta línea
+
+console.log(dog.name);
+console.log(dog.numLegs);
 
 */
 /* */
