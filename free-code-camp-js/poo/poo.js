2c759d7d81303b4aa7c4ee87a19921fa8528a9b1
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-16 07:19:23 -0400

    class 15 fcc-js poo

--- free-code-camp-js/poo/poo.js
+++ free-code-camp-js/poo/poo.js
@@ -674,4 +674,52 @@ Dog.prototype = {
 };
 
 */
+/* class 15 entendiendo de dónde viene el prototipo de un objeto
+
+Así como las personas heredamos genes de nuestros padres, los objetos también heredan su prototype directamente de la función constructor que lo creó. Por ejemplo, aquí el constructor Bird crea el objeto duck:
+
+function Bird(name) {
+  this.name = name;
+}
+
+let duck = new Bird("Donald");
+
+duck hereda su prototype de la función constructor Bird. Puedes mostrar esta relación con el método isPrototypeOf:
+
+Bird.prototype.isPrototypeOf(duck);
+
+Este devolvería true.
+
+-------------------------------------------------------------------------
+ejercicio;
+
+utiliza isPrototypeOf para comprobar el prototype de beagel.
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+// Cambia solo el código debajo de esta línea
+-------------------------------------------------------------------------
+solución;
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+// Cambia solo el código debajo de esta línea
+
+Dog.prototype.isPrototypeOf(beagle);
+
+*/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
 /* */
