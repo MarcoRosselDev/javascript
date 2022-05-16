d4ae7bce8dd9fba0cc827575112f76ebeeede34d
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-16 07:13:54 -0400

    class 14 fcc-js poo

--- free-code-camp-js/poo/poo.js
+++ free-code-camp-js/poo/poo.js
@@ -611,5 +611,67 @@ Dog.prototype = {
 };
 
 */
-/* */
+/* class 14 recuerda establecer la propiedad "constructor" al cambiar el prototipo
+
+Hay un efecto secundario crucial de ajustar manualmente el prototipo a un nuevo objeto. ¡Elimina la propiedad constructor! Esta propiedad puede ser usada para verificar cuál función de constructor creó la instancia. Sin embargo, dado que la propiedad ha sido sobrescrita, ahora devuelve resultados falsos:
+
+duck.constructor === Bird;
+duck.constructor === Object;
+duck instanceof Bird;
+
+En orden, estas expresiones se evaluarían como false, true y true.
+
+Para solucionar esto, cada vez que un prototipo se establece de forma manual a un nuevo objeto, recuerda definir la propiedad constructor:
+
+Bird.prototype = {
+  constructor: Bird,
+  numLegs: 2,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name); 
+  }
+};
+
+-------------------------------------------------------------------------
+ejercicio;
+
+define la propiedad constructor en Dog prototype.
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Cambia solo el código debajo de esta línea
+Dog.prototype = {
+
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
+-------------------------------------------------------------------------
+solcuión;
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Cambia solo el código debajo de esta línea
+Dog.prototype = {
+  constructor: Dog,
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
+
+*/
 /* */
