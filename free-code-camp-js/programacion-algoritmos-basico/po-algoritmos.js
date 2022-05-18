226b1214b99fc2d921d6cd5fa1d2f54636061a6a
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-17 22:15:35 -0400

    class 9 fcc-js algoritmos exercise

--- free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
+++ free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
@@ -228,7 +228,29 @@ function truncateString(str, num) {
 }
 
 */
-/* */
+/* class 9 buscadores guardianes
+
+Crea una función que recorra un arreglo arr y devuelva el primer elemento que pase un "detector de verdad". Esto significa que dado un elemento x, el "detector de verdad" es pasado si func(x) es true. Si ningún elemento pasa la prueba. la función debería devolver undefined.
+
+-------------------------------------------------------------------
+solución;
+
+function findElement(arr, func) {
+  let num = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+*/
 /* */
 /* */
 /* */
