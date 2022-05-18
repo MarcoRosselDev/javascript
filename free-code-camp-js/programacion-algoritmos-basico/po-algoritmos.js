4df8bf41bd2e826d41239efa5a5831146e8a0161
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-17 22:40:14 -0400

    class 11 fcc-js algoritmos exercise

--- free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
+++ free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
@@ -269,7 +269,27 @@ function booWho(bool) {
 booWho(null);
 
 */
-/* */
+/* class 11 haz que la primera letra de una palabra este en mayúscula
+
+Devuelve la cadena proporcionada con la primera letra de cada palabra en mayúsculas. Asegúrate de que el resto de la palabra esté en minúsculas.
+
+Como propósito de este ejercicio, debes también usar mayúsculas conectando palabras como the y of.
+
+----------------------------------------------------------------
+solución;
+
+function titleCase(str) {
+  const newTitle = str.split(" ");
+  const updatedTitle = [];
+  for (let st in newTitle) {
+    updatedTitle[st] = newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+  }
+  return updatedTitle.join(" ");
+}
+
+titleCase("I'm a little tea pot");
+
+*/
 /* */
 /* */
 /* */
