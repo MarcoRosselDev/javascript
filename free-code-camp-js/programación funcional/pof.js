75770bd7539810592ed1ca01f493e083cff67df1
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-17 21:26:37 -0400

    class 6 fcc-js pof

--- free-code-camp-js/programación funcional/pof.js
+++ free-code-camp-js/programación funcional/pof.js
@@ -251,7 +251,49 @@ function incrementer(num) {
 }
 
 */
-/* */
+/*6 factoriza variables globales por fuera de funciones
+
+Hasta ahora, hemos visto dos principios distintos para la programación funcional:
+
+1-  No alteres una variable u objeto: crea nuevas variables y objetos y devuélvelos, si es necesario, desde una función. Pista: usar algo como const newArr = arrVar, donde arrVar es un arreglo, simplemente creará una referencia a la variable existente y no una copia. Así que cambiar un valor en newArr cambiaría el valor en arrVar.
+
+2-  Declara parámetros de función: cualquier cálculo dentro de una función depende sólo de los argumentos pasados a la función y no en ningún objeto o variable global.
+
+Añadir uno a un número no es muy emocionante, pero podemos aplicar estos principios cuando trabajamos con arreglos u objetos más complejos.
+
+------------------------------------------------------------------
+ejercicio;
+
+Reescribe el código para que el arreglo global bookList no sea cambiado dentro de ninguna de las funciones. La función add debe agregar el bookName dado al final del arreglo pasado a esta y devolver un nuevo arreglo (lista). La función remove debe eliminar el bookName dado del arreglo pasado a esta.
+
+Nota: ambas funciones deben devolver un arreglo y cualquier nuevo parámetro debe ser añadido antes del parámetro bookName.
+
+------------------------------------------------------------------
+solución;
+
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+function add(arr, bookName) {
+  let newArr = [...arr]; 
+  newArr.push(bookName); 
+  return newArr; 
+}
+
+function remove(arr, bookName) {
+  let newArr = [...arr]; 
+  if (newArr.indexOf(bookName) >= 0) {
+    newArr.splice(newArr.indexOf(bookName), 1); 
+    return newArr; 
+  }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
+
+*/
 /* */
 /* */
 /* */
