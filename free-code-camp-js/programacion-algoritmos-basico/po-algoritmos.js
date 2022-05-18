45f52d66b6da2bd87350d0777a4a2ffc5b325ee4
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-17 22:45:13 -0400

    class 12 fcc-js algoritmos exercise

--- free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
+++ free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
@@ -290,7 +290,30 @@ function titleCase(str) {
 titleCase("I'm a little tea pot");
 
 */
-/* */
+/* class 12 cortar y rebanar
+
+Tienes dos arreglos y un índice.
+
+Copia cada elemento del primer arreglo en el segundo arreglo, en orden.
+
+Comienza insertando elementos en el índice n del segundo arreglo.
+
+Devuelve el arreglo resultante. Los arreglos de entrada deben permanecer iguales luego de que se ejecute la función.
+
+-----------------------------------------------------------------
+solución;
+
+function frankenSplice(arr1, arr2, n) {
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+*/
 /* */
 /* */
 /* */
