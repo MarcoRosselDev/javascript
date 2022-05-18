0e24524782cbe619e70e52035398f7acbf66d5d1
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-17 23:40:35 -0400

    class 14 fcc-js algoritmos exercise

--- free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
+++ free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
@@ -336,7 +336,31 @@ function bouncer(arr) {
 bouncer([7, "ate", "", false, 9]);
 
 */
-/* */
+/* class 14 dónde pertenezco
+
+Devuelve el índice mas bajo en el que un valor (segundo argumento) debe ser insertado en un arreglo (primer argumento) una vez que éste haya sido ordenado. El valor devuelto debe ser un número.
+
+Por ejemplo, getIndexToIns([1,2,3,4], 1.5) debe devolver 1 por que este valor es más grande que 1 (índice 0), pero menor que 2 (índice 1).
+
+De esta forma, getIndexToIns([20,3,5], 19) debe devolver 2 porque una vez ordenado el arreglo, éste se verá así [3,5,20] y 19 es menor que 20 (índice 2) y mayor que 5 (índice 1).
+
+-----------------------------------------------------------------
+solución;
+
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num)
+      return i;
+  }
+
+  return arr.length;
+}
+
+getIndexToIns([40, 60], 50);
+
+*/
 /* */
 /* */
 const str = 'Life, the universe and everything. Answer:';
