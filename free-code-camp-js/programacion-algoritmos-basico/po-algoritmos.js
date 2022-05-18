5f90209b9189605e4281d5a1a539d2bae8a64b78
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-17 23:47:24 -0400

    class 15 fcc-js algoritmos exercise

--- free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
+++ free-code-camp-js/programacion-algoritmos-basico/po-algoritmos.js
@@ -361,7 +361,31 @@ function getIndexToIns(arr, num) {
 getIndexToIns([40, 60], 50);
 
 */
-/* */
+/* class 15 mutaciones
+
+Devuelve true si la cadena de caracteres en el primer elemento del arreglo contiene todas las letras de la cadena en el segundo elemento del arreglo.
+
+Por ejemplo, ["hello", "Hello"], debe devolver true porque todas las letras de la segunda cadena están presentes en la primera, ignorando mayúsculas o minúsculas.
+
+Los argumentos ["hello", "hey"] deben devolver false porque la cadena hello no contiene y.
+
+Finalmente, ["Alien", "line"], debe devolver true porque todas las letras de line están presentes en Alien.
+
+---------------------------------------------------------------
+solución;
+
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]);
+
+*/
 /* */
 const str = 'Life, the universe and everything. Answer:';
 
