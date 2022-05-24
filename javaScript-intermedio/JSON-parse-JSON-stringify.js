2d35e315bd0d7357e5353bc90db131f1a8474e5f
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-24 10:26:17 -0400

    javaScript intermedio class 7-2

--- javaScript-intermedio/JSON-parse-JSON-stringify.js
+++ javaScript-intermedio/JSON-parse-JSON-stringify.js
@@ -4,3 +4,16 @@ const obj1 = {
     d:{e: "e",
        f: "f"},
 };
+
+//si quiciera copiar este objeto y luego modificarlo usamos
+
+const obj2 = JSON.stringify(obj1);
+
+console.log(obj2);
+
+// nos imprime:     {"a":"b","c":"c","d":{"e":"e","f":"f"}} que es un string
+//luego tenemos JSON.parse para combertir un string en un objeto.
+
+const obj3 = JSON.parse(obj2);
+
+console.log(obj3);
