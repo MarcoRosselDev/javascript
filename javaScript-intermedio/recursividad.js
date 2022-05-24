2f0be9536840c46f9494be47ba7ee53600614b7b
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-24 12:28:57 -0400

    javaScript intermedio class 8-3

--- javaScript-intermedio/recursividad.js
+++ javaScript-intermedio/recursividad.js
@@ -1,8 +1,19 @@
 const numeritos = [0, 1, 3, 6, 7, 4, 2, 0, 7];
 
-let numerito = 0; 
+// let numerito = 0; 
 
-for (let index = 0; index < numeritos.length; index++) {
-    numerito = numeritos[index];
-    console.log({index, numerito});
+// for (let index = 0; index < numeritos.length; index++) {
+//     numerito = numeritos[index];
+//     console.log({index, numerito});
+// }
+
+function recursiva(parametro) {
+    if(parametro.length != 0) {
+        const firstNumber = parametro[0];
+        console.log(firstNumber);
+        parametro.shift();
+        return recursiva(parametro);
+    }
 }
+
+console.log(recursiva(numeritos));
