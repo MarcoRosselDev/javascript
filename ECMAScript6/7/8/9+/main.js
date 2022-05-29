81f496094c4129646f392ab86b75beffdf46b95d
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-29 14:49:05 -0400

    ECMAScript 6 class 2-2

---
+++ ECMAScript6/7/8/9+/main.js
@@ -0,0 +1,8 @@
+let hello = "hello";
+let world = "world";
+
+let fraseEpica = hello + '' + world;
+console.log(fraseEpica);
+
+let fraseEpica2 = `${hello} texto de prueba ${world} `
+console.log(fraseEpica2);
