a9ff5dae7607a3464532480eecf1ce35947b31b0
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-29 16:46:30 -0400

    ECMAScript 6 class 3

--- ECMAScript6/7/8/9+/main.js
+++ ECMAScript6/7/8/9+/main.js
@@ -6,3 +6,12 @@ console.log(fraseEpica);
 
 let fraseEpica2 = `${hello} texto de prueba ${world} `
 console.log(fraseEpica2);
+
+let lorem = "teniamos una frase epica y luego \n"
++ "teniamos que usar la n con el \ para un salto de línea."
+
+let lorem2 = `tenemos que usar la comilla fransesa
+y no es nesesario la n y el \ para saltar la línea.`
+
+console.log(lorem);
+console.log(lorem2);
