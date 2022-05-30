bcd6f9f04309b7c5c772a6c2b6da62e216d18b89
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-29 20:38:20 -0400

    ECMAScript 6 class 5

--- ECMAScript6/7/8/9+/main.js
+++ ECMAScript6/7/8/9+/main.js
@@ -68,3 +68,21 @@ const names2 = [
 
 let listaNombres2 = names2.map(item => console.log(item.name));
 let listaEdades2 = names2.map(item => console.log(item.age));
+
+//---------------------------------------------
+
+class calculator {
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    } 
+}
+
+
+const calc = new calculator();
+console.log(calc.sum(2, 4));
