af4c539639fabf9ab34a0bca92e0e001668060ed
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-30 12:45:46 -0400

    ECMAScript 6 class 6

---
+++ ECMAScript6/7/index.js
@@ -0,0 +1,12 @@
+//junio 2016
+//metodo include se trabaja con arreglos o strings, nos permite saber si hay un elemento x dentro de un string.
+
+let numeros = [1, 2, 3, 4, 5, 9];
+
+if (numeros.includes(7)) {
+    console.log('si se encuentra el numero 7');
+} else {
+    console.log('no se encuentra el numero 7');
+}
+
+//si aplico 7 imprime else si aplico 5 imprime if.
