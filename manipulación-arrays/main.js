b0e5db85e723a4955253920a1b0ad7f0cdaf750b
Marco Rossel <andresmarcorossel@gmail.com>
2022-05-31 18:27:14 -0400

    manipulación arrays.js class 2-3

---
+++ manipulación-arrays/main.js
@@ -0,0 +1,11 @@
+const letters = ['a', 'b', 'c'];
+letters.forEach(element => console.log(element));
+
+
+const letters2 = ['a', 'b', 'c'];
+for (let index = 0; index < letters2.length; index++) {
+    const element = letters2[index];
+    console.log(element)
+}
+
+//hacen lo mismo, 'recorren los elementos del arreglo y los imprimen en consola' 
