1f230060cac9694939dc19f2411d28db39ddd60b
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-01 20:26:44 -0400

    asincronismo-JS class 7-2

--- asincronismo-JavaScript/src/promise/index.js
+++ asincronismo-JavaScript/src/promise/index.js
@@ -1,9 +1,15 @@
 const somethingWillHappen = () => {
     return new Promise((resolve, reject) => {
-        if(true) {
+        if(false) {
             resolve('hey');
         } else {
             reject('buuuuu');
         }
     });
 };
+
+somethingWillHappen()
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
+//si ejecutamos con true = hey
+//si ejecutamos con false = buuuuu
