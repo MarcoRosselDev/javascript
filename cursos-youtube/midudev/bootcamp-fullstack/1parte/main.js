a1016e3529793ab05106f8ba363d25696d365002
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-03 23:38:04 -0400

    bootcamp midudev 1 notes-part1-1-4

--- cursos-youtube/midudev/bootcamp-fullstack/1parte/main.js
+++ cursos-youtube/midudev/bootcamp-fullstack/1parte/main.js
@@ -1,1 +1,15 @@
 console.log('probando el javaScript');
+
+//vemos los 3 tipos de variables en js
+
+let name = 'marco';
+const lastName = 'rossel';
+var noUsar = true;
+
+// ¿ cuales son las diferencias ?
+
+//let = permitir
+//lo que tiene let es que puedo reasignarle el valor 
+
+//const = contante
+//en el caso de const no se puede reasignar una variable. nos lanzaria un error en consila.
