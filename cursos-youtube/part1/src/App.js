525eefed746fb4741c68e3b156becdd117187ce1
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-04 23:42:29 -0400

    bootcamp midudev 1 notes-part2-3

--- cursos-youtube/part1/src/App.js
+++ cursos-youtube/part1/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-/* 
+/*  class 1
 function App() {
   return (
     <div className="App">
@@ -9,7 +9,7 @@ function App() {
 }
 */
 
-//se puede declarar o inicializar la variable por ejemplo:
+//class 2-----------------------se puede declarar o inicializar la variable por ejemplo:
 
 // const App = () => {
 //   //podemos meter el msn en una variable:
@@ -27,6 +27,31 @@ function App() {
 //   );
 // }
 // export default App;
+
+/* class 3
+
+// const App = () => {
+//   const mensaje = 'wena cabros.';
+//   const a = 6;
+//   const b = 4;
+
+//   return (
+//     <div className="App">
+//       {mensaje + ", esoy probando el texto"}
+//       <br/>
+//       {a ** b}
+//     </div>
+//     // los objetos no son validos como hijos de react. por lo que solo podemos trabajar con cosas que se puedan pintar como numeros, textos.
+
+//     //si queremos jugar con algo como por ejemplo {new Date()} no lo reconoseria, entonces lanzaria error.
+//   )
+// }
+// export default App;
+
+*/
+
+//----------------------------------------------------------
+//class 4
 
 const App = () => {
   const mensaje = 'wena cabros.';
