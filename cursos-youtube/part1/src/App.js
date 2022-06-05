38fb6d6a3733702478d6947833049afa0f39474d
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-04 23:26:52 -0400

    bootcamp midudev 1 notes-part2

--- cursos-youtube/part1/src/App.js
+++ cursos-youtube/part1/src/App.js
@@ -11,18 +11,31 @@ function App() {
 
 //se puede declarar o inicializar la variable por ejemplo:
 
+// const App = () => {
+//   //podemos meter el msn en una variable:
+//   const mensaje = 'wena cabros.';
+
+//   return (
+//     <div className="App">
+//       {mensaje + `, esoy probando el texto
+//       para concatenarlo a una variable, y usando la
+//        comilla francesa para no romper 
+//       el codigo al hacer saltos de linea` }
+
+//     {/* usamos las llaves para evaluar  */}
+//     </div>
+//   );
+// }
+// export default App;
+
 const App = () => {
-  //podemos meter el msn en una variable:
   const mensaje = 'wena cabros.';
+  const a = 5;
+  const b = 2;
 
   return (
     <div className="App">
-      {mensaje + `, esoy probando el texto
-      para concatenarlo a una variable, y usando la
-       comilla francesa para no romper 
-      el codigo al hacer saltos de linea` }
-
-    {/* usamos las llaves para evaluar  */}
+      {mensaje + ", esoy probando el texto"}
     </div>
   );
 }
