419e5ad9bc13ba7a0fadc5f995128bad639b9b7e
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-05 21:32:30 -0400

    ingles-A2-escritura c-2

--- ingles/A2-escritura/notes.js
+++ ingles/A2-escritura/notes.js
@@ -1,4 +1,20 @@
-/*class 2
+/*class 2 | capitalization | abbreviation | indentation
+
+"capitalización, abreviatura y sangría"
+
+parecen detalles pero haran la diferencia en la comprención de la lectura de textos
+----------------------------------------------
+capitalization:
+    es cuando escribimos con mayúscula la primera letra de la palabra, las demas letras estan en minusculas.
+
+regla {
+    siempre debes comenzar una oración con una letra mayúscula
+}
+
+ejemplo [
+    My name is Marco
+]
+-----------------------------------------------
 
 
 
