ab8824680e5db46f294303459b7f0b77176a0e0a
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-06 02:18:35 -0400

    manipulación arrays class 2-2

--- manipulación-arrays/server/01-for-each.js
+++ manipulación-arrays/server/01-for-each.js
@@ -2,7 +2,23 @@ const letters = ['a','b','c','d','e'];
 
 // primero veremos una iteración normal de este arreglo llamado letras.
 
-for (let i = 0;i < array.letters; i++) {
-    const elemento = array[i];
-    console.log(elemento);
+for (let i = 0;i < letters.length; i++) {
+    const elemento = letters[i];
+    console.log('for', elemento);
 }
+
+//ahora usando metodos de arrays para hacer el codigo más reducido.
+
+letters.forEach(item => console.log('forEach', item))
+
+/*  solución:
+for a
+for b
+for c
+for d
+for e
+forEach a
+forEach b
+forEach c
+forEach d
+forEach e */
