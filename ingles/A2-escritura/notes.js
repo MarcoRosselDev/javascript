18e875a8c2eaa9eace0f5862cc38173d6f4efa68
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-05 21:44:11 -0400

    ingles-A2-escritura c-2-2

--- ingles/A2-escritura/notes.js
+++ ingles/A2-escritura/notes.js
@@ -15,6 +15,36 @@ ejemplo [
     My name is Marco
 ]
 -----------------------------------------------
+regla2 {
+    siempre la palabra yo "I" se escribe con mayúscula, no importa si esta al principio, en el medio o en el final de la oración.
+}
+
+ejemplo2 [
+    My sister and I watch TV
+]
+-----------------------------------------------
+regla3 {
+    los nombres y títulos siempre se escriben con mayúescula.
+    subregla3 {
+        ciudades y países, igual.
+        subregla4 {
+            nacionalidades y idiomas +.
+            subregla5 {
+                semanas,
+                meses,
+                festivos +.
+            }
+        }
+    }
+}
+
+ejemplo3-4-5 [
+    This is Mr. Jones
+    I live in Concepción, Chile.
+    I'am Chilean and I speak Spanish
+    Today id Sunday, February 14th. It's Valentine's Day!
+]
+
 
 
 
