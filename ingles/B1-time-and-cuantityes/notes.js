c5089d78e42aaa34de4482851397dc65edb1f82c
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-07 07:39:04 -0400

    ingles-B1-time and quantity

---
+++ ingles/B1-time-and-cuantityes/notes.js
@@ -0,0 +1,31 @@
+/*1 examen
+
+instructions:
+Read the sentences carefully and select the word that works best in the context of the sentence.
+
+1   There's_____apple on the table.
+    a.Some
+    b.a
+    c.an---
+
+2   There is_____water in the fridge
+    a.much
+    b.many
+    c.a lot of---
+    d.a
+    e.a little
+
+3   Would you like________grapes?
+    a.much
+    b.any
+    c.many
+    d.a little
+    e.some---
+
+
+*/
+/* */
+/* */
+/* */
+/* */
+/* */
