3635835bb2255d338340e8d95ece86b8fba4bad8
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-07 07:47:11 -0400

    ingles-B1-time and quantity exam

--- ingles/B1-time-and-cuantityes/notes.js
+++ ingles/B1-time-and-cuantityes/notes.js
@@ -22,6 +22,27 @@ Read the sentences carefully and select the word that works best in the context 
     d.a little
     e.some---
 
+4   Which words means 75% of the people
+    a.most of---
+    b.some of
+    c.a lot of
+    d.half of 
+    e.anyone
+
+5   _________needs to feel loved
+    a.somebody
+    b.anyone    **
+    c.everybody ---
+    d.no one    *
+    e.everything **
+
+6   I can't find my phone__________
+    a.anywhere  *
+    b.everywhere---
+    c.somewhere
+    d.nowhere   **
+    e.everything ** 
+
 
 */
 /* */
