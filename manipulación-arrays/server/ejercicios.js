8a00b4de9204b1807471cfa987e1743ccceef1f1
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-07 07:02:06 -0400

    manipulación arrays class 5

--- manipulación-arrays/server/ejercicios.js
+++ manipulación-arrays/server/ejercicios.js
@@ -1,12 +1,29 @@
-//2 tienes un array de objetos que representan datos de personas con los siguientes atributos:
+/*1 devolver array con cada elemento multiplicado por 2
 
-// neme
-// lastName
-// age
+solución:
 
-// el ejercicio es retornar un array de strings con los nombres, la solución debería tener un input y output como los siguientes:   
+function solution(array) {
+ 	// Tu código aquí 👈 
+	const newArray = array.map(elemento => elemento * 2);
+	return newArray
+}; 
+*/
 
-/* solution([
+/*2 Transforma un Array de objetos a un Array de strings
+
+function solution(array) {
+ 	// Tu código aquí 👈 
+}; 
+
+En este desafío tienes un array de objetos que representan datos de personas con los siguientes atributos:
+
+name
+lastName
+age
+El reto aquí es retornar un array de strings con los nombres, la solución debería tener un input y output como los siguientes:
+
+Input
+solution([
   {
     name: 'Nicolas',
     lastName: 'Molina',
@@ -18,6 +35,37 @@
     age: 19
   },
   ...
-]);*/
+]);
 
-//Output = ['Nicolas', 'Valentina']
+Output
+
+['Nicolas', 'Valentina']
+
+
+
+*/
+
+solution([
+  {
+    name: 'Nicolas',
+    lastName: 'Molina',
+    age: 28
+  },
+  {
+    name: 'Valentina',
+    lastName: 'Molina',
+    age: 19
+  }
+]);
+
+function solution(array) {
+  const j = [];
+
+  array.map(element => {
+    j.push(element.name)
+  });
+
+  return j
+}; 
+
+solution();
