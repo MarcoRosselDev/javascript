b7c320fa734808ea37a72fedae2dbe3c5651dd8a
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-07 23:19:32 -0400

    FCC frases 07 06 23

---
+++ frases-FCC.js
@@ -0,0 +1,5 @@
+/* muhammad alí
+"Imposible es solo una gran palabra lanzada por hombres pequeños a quienes les resulta más fácil vivir en el mundo que se les ha dado que explorar el poder que tienen para cambiarlo. Imposible no es un hecho. es una opinion Imposible no es una declaración. Es un atrevimiento. Imposible es potencial. Imposible es temporal. Nada es imposible."
+------------------------------------------------------
+"Impossible is just a big word thrown around by small men who find it easier to live in the world they've been given than to explore the power they have to change it. Impossible is not a fact. It's an opinion. Impossible is not a declaration. It's a dare. Impossible is potential. Impossible is temporary. Impossible is nothing."
+*/
