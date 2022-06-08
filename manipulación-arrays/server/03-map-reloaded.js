704a79ff32c8ae4ba127e2c66478e587aca4d598
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-08 05:50:30 -0400

    manipulación arrays class 6-5

--- manipulación-arrays/server/03-map-reloaded.js
+++ manipulación-arrays/server/03-map-reloaded.js
@@ -25,8 +25,8 @@ const orders = [
 
 const repuesta = orders.map(item => item.total)
 
-console.log(orders);
-console.log(repuesta);
+// console.log(orders);
+// console.log(repuesta);
 
 //cuando use las {} me salia undefined en respuesta. ????
 
@@ -38,4 +38,23 @@ solo agregar un nuevo atributo a cada uno de los objetos
 
 la respuesta a los braquet {} que no me funcionaban era por que es para cuando nesecitamos más espacio, en ese caso usaremos un retorn con lo que queremos de vuelta.
 en caso de que este todo en una linea no esamos braquets y tmpoco usamos return.
+*/
+
+const respuesta2 = orders.map((item) => {
+  item.max = 0.19;
+  return item;
+});
+
+console.log(respuesta2);
+console.log(orders);
+
+/*resulta que modificamos el objeto original "orders"
+por que?
+
+1 para empezar solo pasa esto con objetos.
+  si hacemos esto con numeros o arrays funcionara a la perfeción.
+
+2 el problema es que si copiamos algo y le asignamos un nuevo valor 
+  de variable todo bien, pero si a este por dentro usamos un valor reservado en memoria, cometeremos el error de alterar este.
+
 */
