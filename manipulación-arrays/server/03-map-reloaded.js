77a6ab33750a4fe31aa07d138d723df9339a3feb
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-08 03:37:32 -0400

    manipulación arrays class 6-2

---
+++ manipulación-arrays/server/03-map-reloaded.js
@@ -0,0 +1,24 @@
+const orders = [
+    {
+      customerName: "Nicolas",
+      total: 60,
+      delivered: true,
+    },
+    {
+      customerName: "Zulema",
+      total: 120,
+      delivered: false,
+    },
+    {
+      customerName: "Santiago",
+      total: 180,
+      delivered: true,
+    },
+    {
+      customerName: "Valentina",
+      total: 240,
+      delivered: true,
+    },
+  ];
+
+//   lo que quiero es sacar x datos en este caso los totales para agregarlos a un nuevo array, que ahora sera solo de numeros.
