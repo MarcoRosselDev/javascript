556264dea8d760e7462410073d05fd6ee7fae3f0
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-09 05:32:07 -0400

    manipulación arrays class 8-2

---
+++ manipulación-arrays/server/04-filter.js
@@ -0,0 +1,10 @@
+const words = ['spray', 'limit', 'elite', 'exuberant'];
+
+const newArray = [];
+
+for (let i = 0;i < words.length; i++) {
+    const element = words[i];
+    if () {
+        
+    }
+}
