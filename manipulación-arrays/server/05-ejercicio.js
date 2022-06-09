d45b631ba2fbdcd98611996bb6b185848f753260
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-09 07:13:30 -0400

    manipulación arrays class 10-3

---
+++ manipulación-arrays/server/05-ejercicio.js
@@ -0,0 +1,27 @@
+/*
+Tienes un array de números y debes retornar la suma de todos los valores en él.
+
+La solución debería tener un input y output como los siguientes:
+
+solution([1, 1, 1]);
+solution([2, 4, 8]);
+
+Output
+
+3
+14
+
+*/
+
+function solution(numbers) {
+	let sum = 0;
+
+	for (let i = 0; i < numbers.length; i++) {
+		const element = numbers[i];
+		sum = sum + element;
+	}
+
+	const rta = numbers.reduce((sum, element) => sum + element);
+
+	return rta;
+}; 
