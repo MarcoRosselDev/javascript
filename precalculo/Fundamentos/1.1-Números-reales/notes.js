0f54cbb82d52dd042406bc71c23cfa7bbc488ca2
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-09 01:45:31 -0400

    precalculo 1-1.1

---
+++ precalculo/Fundamentos/1.1-Números-reales/notes.js
@@ -0,0 +1,21 @@
+/*
+ tenemos las propiedades basicas del algebra
+ 
+ conmutatiba
+ asociativa
+ distributiva
+
+ para probar estas propiedades se pide hacer el ejercicio 15
+--------------------------------------------------------
+ejercicio 15:
+
+(5x + 1)3 = 15x + 3
+
+probemos aplicar esta propiedad en JavaScript
+--------------------------------------------------------
+--------------------------------------------------------
+
+
+
+
+*/
