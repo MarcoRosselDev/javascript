655ec119a5f04f2ffe805b75f127700f18c3c31c
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-10 11:04:22 -0400

    manipulación arrays class 12-2

---
+++ manipulación-arrays/server/06-reduce-reloaded.js
@@ -0,0 +1,13 @@
+const items = [1, 3, 2, 3];
+
+//queremos un .reduce que itere por el array y nos diga cuantos 1,2,3... tiene el array.
+/*se espera algo asi:
+{
+    1:1,
+    2:1,
+    3:2
+}
+
+el atributo sera el elemento y el valor es el número de veces que se repite en el arrat
+
+*/
