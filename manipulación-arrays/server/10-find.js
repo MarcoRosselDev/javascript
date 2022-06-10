a74a6a39909640dec40d95fd38f5eeb37c2b75ca
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-10 13:39:33 -0400

    manipulación arrays class 17-3

--- manipulación-arrays/server/10-find.js
+++ manipulación-arrays/server/10-find.js
@@ -4,9 +4,18 @@ let respuesta = undefined;
 
 for (let i = 0;i < numeros.length; i++) {
     const elemento = numeros[i];
-    if (elemento === 30) {
+    if (elemento === 52) {
         respuesta = elemento;
         break
     }
 }
-console.log(respuesta);
+// console.log(respuesta);
+
+//respuesta = 52
+
+//ahora con .find
+
+const respuesta2 = numeros.find(item => item === 22);
+console.log(respuesta2);
+
+//esta de pana, responde 22
