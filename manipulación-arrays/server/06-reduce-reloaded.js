1571f2c7bf4f4249e212304b66ce7f7c0039fbce
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-10 11:09:54 -0400

    manipulación arrays class 12-3

--- manipulación-arrays/server/06-reduce-reloaded.js
+++ manipulación-arrays/server/06-reduce-reloaded.js
@@ -11,3 +11,16 @@ const items = [1, 3, 2, 3];
 el atributo sera el elemento y el valor es el número de veces que se repite en el arrat
 
 */
+
+const rta = items.reduce((obj, item) => {
+    if(!obj[item]) {
+        obj[item] = 1;
+    } else {
+        obj[item] = obj[item] + 1;
+    }
+    return obj;
+}, {});
+
+console.log(rta);
+
+//consola = { '1': 1, '2': 1, '3': 2 }
