53212f8f846276279b803ae884eb04ab94a3830e
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-11 05:23:20 -0400

    manipulación arrays class 22-3

--- manipulación-arrays/server/13-concat.js
+++ manipulación-arrays/server/13-concat.js
@@ -8,9 +8,31 @@ for(let i = 0;i < elem2.length; i++) {
     resp1.push(item);
 }
 
-console.log(resp1);
+// console.log(resp1);
 
 //[
 //  1, 1, 2, 2, 3,
 //  3, 4, 4, 5, 5
 //]
+
+const resp2 = elem1.concat(elem2);
+console.log(resp2);
+
+/* repuesta
+[
+  1, 1, 2, 2, 3,
+  3, 4, 4, 5, 5
+]
+*/
+
+//tambien es valido:
+
+const resp3 = [...elem1, ...elem2];
+console.log(resp3);
+
+/* repuesta
+[
+  1, 1, 2, 2, 3,
+  3, 4, 4, 5, 5
+]
+*/
