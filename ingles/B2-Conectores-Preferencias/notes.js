a73c338233a2ec8dbdffd9ab6cc4c00273a7882e
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-11 10:53:12 -0400

    ingles-B1-conectores class 2-2

--- ingles/B2-Conectores-Preferencias/notes.js
+++ ingles/B2-Conectores-Preferencias/notes.js
@@ -1,6 +1,15 @@
 /*2 Live events
 
+usamos la forma de presente simple para describir o narrar eventos que estan ocurriendo como un partido de futbol o una pelicula.
 
+ejercico:
+usaremos los siguientes verbos...
+
+passes      pase
+nutmegs     nuesmoscada = shorita/caño/
+runs        carreras
+shoots      dispara
+scores      puntuaciones
 
 */
 /* */
