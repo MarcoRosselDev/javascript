961ebb9114719c5b580bb7e25445441a590a5d2d
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-11 10:59:19 -0400

    ingles-B1-conectores class 2-3

--- ingles/B2-Conectores-Preferencias/notes.js
+++ ingles/B2-Conectores-Preferencias/notes.js
@@ -11,6 +11,21 @@ runs        carreras
 shoots      dispara
 scores      puntuaciones
 
+notar que a los verbos se les agrega una +s al final para relatar el evento en tercera persona
+
+si estemos hablando de he/she/it siempre +s al verbo o +es.
+
+run > runs
+shoot > shoots
+
+------------------------------------------------
+
+He passes the ball to Ronaldo.
+Ronaldo nutmegs and rounds past the defender.
+This is very dangerous.
+He shoots and scores.
+It's a brilliant finish and he knows it.
+
 */
 /* */
 /* */
