2db78087561889b7e787873b981eab58fd9254a5
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-13 13:18:11 -0400

    consumo API class 1

---
+++ 11_consumo_API/index.js
@@ -0,0 +1,14 @@
+/* 1 API REST:
+API REST 🌐
+API: Application Program Interface -> Interfaz
+Una interfaz es el medio por el cual la computadora se conecta con los humanos. Es lo visible para el usuario que interactúe con una app, por ejemplo.
+
+API REST
+Es el medio por el cual se comunican computadoras con otras computadoras. O también podemos pensarlo como robots con robots 🤖🔁🤖. Puede ser backend con frontend, backend con backend, etc.
+
+REST: Representational State Transfer
+
+A diferencia de una página web, que se muestra con imágenes, textos formateados, etc.; la llamada de una API REST se devuelve en un archivo JSON.
+
+Aunque pocas, algunas aplicaciones todavía usan el método SOAP para mandar información entre computadoras. Sin embargo, actualmente REST está dominando su aplicación.
+*/
