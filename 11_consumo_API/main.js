b2901f02528662ea61640e1811f126afd79e56a3
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-13 16:08:09 -0400

    consumo API class 4-2

--- 11_consumo_API/main.js
+++ 11_consumo_API/main.js
@@ -5,10 +5,11 @@ const API_URL = 'https://api.thecatapi.com/v1/images/search';
 
 
 async function reload() {
-    fetch(API_URL)
-    .then(res => res.json())
-    .then(dat => {
-        const img = document.querySelector('img');
-        img.src = dat[0].url;
-    });
+    const res = await fetch(API_URL);
+    const data = await res.json();
+
+    const img = document.querySelector('img');
+    img.src = data[0].url;
 }
+
+reload();
