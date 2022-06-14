d4c03198f962dc4a0150015052238ff83e614ff1
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-14 14:24:59 -0400

    consumo API class 8-4

--- 11_consumo_API/main.js
+++ 11_consumo_API/main.js
@@ -1,18 +1,25 @@
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=a196477d-cc81-41c3-bc61-85544de3a482';
-const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favorites?limit=2&api_key=a196477d-cc81-41c3-bc61-85544de3a482';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=a196477d-cc81-41c3-bc61-85544de3a482';
 
-
+const spanError = document.getElementById('error');
 
 async function loadRandomCat() {
     const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
+    console.log('Random')
+    console.log(data)
 
+    if (res.status !== 200) {
+        spanError.innerHTML = 'Hubo un error: ' + res.status;
+    } else {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
 
 
     img1.src = data[0].url;
     img2.src = data[1].url;
+    }
+    
 }
 
 async function loadFavoritesCat() {
