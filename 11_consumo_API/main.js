f2fa1eb45c20600e55572eb493c5ae8466c3d458
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-14 13:40:56 -0400

    consumo API class 8-3

--- 11_consumo_API/main.js
+++ 11_consumo_API/main.js
@@ -1,9 +1,10 @@
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=a196477d-cc81-41c3-bc61-85544de3a482';
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=a196477d-cc81-41c3-bc61-85544de3a482';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favorites?limit=2&api_key=a196477d-cc81-41c3-bc61-85544de3a482';
 
 
 
 async function loadRandomCat() {
-    const res = await fetch(API_URL);
+    const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
 
     const img1 = document.getElementById('img1');
@@ -14,4 +15,12 @@ async function loadRandomCat() {
     img2.src = data[1].url;
 }
 
-reload();
+async function loadFavoritesCat() {
+    const res = await fetch(API_URL_FAVORITES);
+    const data = await res.json();
+
+    console.log(data);
+}
+
+loadRandomCat();
+loadFavoritesCat();
