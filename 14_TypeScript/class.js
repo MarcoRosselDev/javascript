4fa603a646bcf61b5a5ff3e2e1235bb6f2e71e64
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-17 14:31:40 -0400

    TypeScript_1 class 3-2

--- 14_TypeScript/class.js
+++ 14_TypeScript/class.js
@@ -17,6 +17,16 @@ TypeScript:
 1   npm install typescript --save-dev
     por que solo lo queremos en nuestro entorno local
 
+2   https://www.toptal.com/developers/gitignore
+    le espesificamos los sistemas que deseamos trabajar
+    windows macOS Linux Node
+    le damos a crear y copiamos y pegamos en .gitignore
+
+3   .gitignore
+    pegamos el documento de www.toptal.com
+
+4   .editorconfig
+    
 */
 /* */
 /* */
