7c684ee2504279b526950befa1633be6b831b941
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-17 14:09:56 -0400

    TypeScript_1 class 2-2

--- 14_TypeScript/class.js
+++ 14_TypeScript/class.js
@@ -1,4 +1,17 @@
-/*2 TypeScript vs JavaScript */
+/*2 TypeScript vs JavaScript
+
+javaScript:
+    "solo te das cuenta de los errores hasta que el código está en ejecución".
+
+TypeScript:
+    typeScript(ES...(javaScript))
+    "corre solo en el editor de codigo, me corrije mientras estoy escribiendo"
+        Static analysis:editor(vscode)
+        Unit tests:pruebas del codigo
+        Integration tests:como funcióna todo en conjunto
+        Code review: normas y practicas de codigo en equipo.
+
+*/
 /* */
 /* */
 /* */
