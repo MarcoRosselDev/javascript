1bfa3d8eae4d9374ba30c877b2da6ccc2e0866a2
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-20 19:35:32 -0400

    Mates.exam 19

--- precalculo/Matematicas.dev/examen.js
+++ precalculo/Matematicas.dev/examen.js
@@ -34,8 +34,20 @@ Sumar la matriz [[1 3] [5 6]] con el escalar 3 (matriz+escalar) da lo mismo que 
 Una buena forma de identificar nuestros errores en el código es a través del:
 
 */
-/* */
-/* */
+/*18 Estadística computacional con python
+
+12/24
+
+¿Una muestra aleatoria tiende a exhibir las mismas propiedades que la población de la cual fue extraída?
+
+*/
+/*19 Estadística computacional con python
+
+10/24
+
+La probabilidad de que estés estudiando en Platzi y de que seas Colombiano, ¿es menor a la probabilidad de que seas Colombiano?
+
+*/
 /* */
 /* */
 /* */
