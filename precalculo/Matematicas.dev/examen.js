8b6dd2676bfe78cdfa631d1d2f5839bd0f53cd0e
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-20 19:33:29 -0400

    Mates.exam 16

--- precalculo/Matematicas.dev/examen.js
+++ precalculo/Matematicas.dev/examen.js
@@ -13,9 +13,27 @@
 El número de personas que viven en un pueblo es de 10000. Si después de una década hay 20000 personas en dicho pueblo. ¿Cuánto tiempopasará para que hayan 50000 habitantes?
 
 */
-/* */
-/* */
-/* */
+/*13 Fundamentos de álgebra lineal con python
+
+5/28
+
+Para calcular la dimensión de un tensor debo usar la instrucción/atributo:
+
+*/
+/*14 Fundamentos de álgebra lineal con python
+
+7/28
+
+Sumar la matriz [[1 3] [5 6]] con el escalar 3 (matriz+escalar) da lo mismo que hacer:
+
+*/
+/*16 Introdución al pensamiento computacional con python
+
+27/31
+
+Una buena forma de identificar nuestros errores en el código es a través del:
+
+*/
 /* */
 /* */
 /* */
