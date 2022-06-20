4869ebb3d33ed259aa2ea1eb095088c996f748b2
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-20 19:26:57 -0400

    Mates.exam 5

--- precalculo/Matematicas.dev/examen.js
+++ precalculo/Matematicas.dev/examen.js
@@ -6,7 +6,13 @@
 (p^q) -> (q^~p)
 
 */
-/* */
+/*5 Ecuaciones diferenciales
+
+39/50
+
+El número de personas que viven en un pueblo es de 10000. Si después de una década hay 20000 personas en dicho pueblo. ¿Cuánto tiempopasará para que hayan 50000 habitantes?
+
+*/
 /* */
 /* */
 /* */
