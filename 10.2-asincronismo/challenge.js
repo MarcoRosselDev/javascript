46b3d69699e06b61ee6e07ddd58c9dc41ddd1aec
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-25 01:25:58 -0400

    asincronismo-2022 class 8

--- 10.2-asincronismo/challenge.js
+++ 10.2-asincronismo/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1';
 
 function fetchData(urlApi, callback) {
@@ -6,7 +6,7 @@ function fetchData(urlApi, callback) {
 
     xhttp.open('GET', urlApi, true);
     xhttp.onreadystatechange = function (event) {
-        if (xhttp.readyStage === 4) {
+        if (xhttp.readyState === 4) {
             /* 0 = no inicializado
             1 = loadin(ahun no se llama el valor sent)
             2 = ya se ejecuto el valor sent
@@ -15,16 +15,16 @@ function fetchData(urlApi, callback) {
             */
            if (xhttp.status === 200) {
             callback(null, JSON.parse(xhttp.responseText));
-           }
-        } else {
-            const error = new Error('Error' + urlApi);
-            return callback(error, null);
+           } else {
+                const error = new Error('Error' + urlApi);
+                return callback(error, null);
+            } 
         }
     }
     xhttp.send();
 }
 
-fetchData(`${API}/products`, function(error1, data1) {
+fetchData(`${API}/products`, function (error1, data1) {
     if (error1) return console.error(error1);
     fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
         if (error2) return console.error(error2);
@@ -33,6 +33,6 @@ fetchData(`${API}/products`, function(error1, data1) {
             console.log(data1[0]);
             console.log(data2.title);
             console.log(data3.name);
-        })
-    })
-})
+        });
+    });
+});
