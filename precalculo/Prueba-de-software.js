8acf225793f191aba84f0a9d638bcfaabd96d4d3
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-26 23:55:03 -0400

    prueba de software class 4

---
+++ precalculo/Prueba-de-software.js
@@ -0,0 +1,26 @@
+/*
+Definición de objetivos
+En esta fase se define el alcance general del software y su papel dentro de una estrategia global o dentro del ecosistema que va a funcionar.
+
+Análisis de los requisitos y su viabilidad
+Se trata de recopilar la mayor cantidad de información posible para evaluar la viabilidad del producto, encontrar posibles restricciones y analizar todos los requisitos del cliente.
+
+Diseño
+Alto nivel: Se trata de realizar un diseño básico que valide la arquitectura de la aplicación.
+Bajo nivel: Es una definición detallada de la estructura de la aplicación basada en el diseño general.
+
+Programación
+Es la implementación de un lenguaje de programación para crear las funciones definidas durante la etapa de diseño.
+
+Pruebas de verificación
+Aunque en todas las fases anteriores se hacen pruebas en esta fase se cubren: pruebas de componentes, integrales y de sistema.
+
+Prueba beta (o validación)
+Se hace para garantizar que el software cumple con las especificaciones originales o también se hacen las pruebas de aceptación.
+
+Implementación
+Se realiza una prueba del sistema implementado para encontrar posibles fallas en la Implementación.
+
+Mantenimiento
+Se hace para todos los procedimientos correctivos (mantenimiento correctivo) y a las actualizaciones secundarias del software (mantenimiento continuo), junto con la actualización de las pruebas.
+*/
