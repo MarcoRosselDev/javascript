186b1e5d3b2057e6e3e2d164d5e6f028b173064e
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-27 00:22:51 -0400

    prueba de software class 6

--- precalculo/Prueba-de-software.js
+++ precalculo/Prueba-de-software.js
@@ -24,3 +24,21 @@ Se realiza una prueba del sistema implementado para encontrar posibles fallas en
 Mantenimiento
 Se hace para todos los procedimientos correctivos (mantenimiento correctivo) y a las actualizaciones secundarias del software (mantenimiento continuo), junto con la actualización de las pruebas.
 */
+
+/* class 6 Principios del testing moderno
+
+Los principios creados por Allan Page y Brenn Jensen es acerca del testing moderno, que es la evolución natural del testing ágil. Veían la necesidad de que el tester y el desarrollador evolucionen en su perspectiva, ambos hacen software y entregan al mismo cliente, por lo tanto deben trabajar más como un equipo y no como entidades separadas. El tester debería enfocarse en la calidad del sowftware, el desarrollador debería enfocarse a desarrollar la solución. El tester va a ayudar al desarrollador a utilizar las mejores herramientas, a entender los procesos de pruebas, a mejorar la calidad de su desarrollo.
+
+Los 7 principios del testing moderno
+
+“Los testers podemos comenzar a pasar de ser los dueños de las pruebas o la calidad, a ser los embajadores de la calidad del producto”.
+
+Nuesta prioridad es mejorar el negocio.
+Nosotros aceleramos al equipo, usamos modelos como Lean Thinking y Teoría de las Restricciones para ayudar a identificar, mitigar, priorizar y mitigar cuellos de botella en el sistema.
+Somos la fuerza para la mejora continua, ayudando al equipo a adaptarse y optimizar para tener éxito, en lugar de proporcionar una red de seguridad para detectar fallas.
+Nos preocupamos profundamente acerca de la cultura de calidad en el equipo, y asesoramos, lideramos y nutrimos al equipo para llevarlos a una cultura de calidad más madura.
+Nosotros creemos que le cliente es el único capaz de juzgar y evaluar la calidad de nuestro producto.
+Nosotros usamos datos de manera extensa y profunda para entender los casos de usos del cliente y entonces cerrar huecos entre hipótesis del producto e impacto del negocio.
+Expandimos las habilidades de testing y el conocimiento en todo el equipo; entendemos que esto reduce o elimina la necesidad de un especialista dedicado al testing.
+
+*/
