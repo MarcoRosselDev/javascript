f3de98f857184a245b671aed07eeccd700cf9cc4
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-27 03:37:30 -0400

    node class 7-2

--- 20-node/callback.js
+++ 20-node/callback.js
@@ -1,9 +1,11 @@
-function soyAsincrona() {
+function soyAsincrona(miCallback) {
     console.log('soy una función asincrona');
     setTimeout(function () {
         console.log('Estoy siendo asincrono')
+        miCallback();
     }, 1000)
 }
-console.log('Iniciando proceso...')
-soyAsincrona();
-console.log('Finalizando proceso...')
+console.log('Iniciando proceso...');
+soyAsincrona(function () {
+    console.log('Finalizando proceso...');
+});
