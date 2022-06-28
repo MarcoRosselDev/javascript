475c421cfa1abe4a19388337baf004289d7aaeb5
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-28 06:20:42 -0400

    ingles b1 infin.. and transit... class 12

--- ingles/B!-verbos-infinitivos-y-transitivos/notes.js
+++ ingles/B!-verbos-infinitivos-y-transitivos/notes.js
@@ -25,7 +25,33 @@ Till :
     2. I'll be out of town till tomorrow.
     Estaré fuera de la ciudad hasta mañana.
 */
-/* */
+/*12 Prohibition -- muar and  mustn't
+
+Must / musn’t sirve para hablar acerca de una regla u orden, algo que es obligatorio de realizar. Puede usarse particularmente para prohibir algo en un lugar.
+
+Ejemplos usando must/musn’t
+What mustn’t we do in the restaurant?
+
+We mustn’t leave without paying.
+We must be respectful to the server.
+We mustn’t throw the food in the air.
+What must we do at school or university?
+
+We must respect the teacher.
+We must do our homework.
+We must be on time.
+What must we do at the airport?
+
+We must bring our passport or identity card?
+We must be on time.
+We must be patient, because sometimes there are a lot of delays.
+What must we do at the park?
+
+We mustn’t throw garbage on the ground.
+We must have fun.
+We mustn’t damage the plants.
+
+*/
 /* */
 /* */
 /* */
