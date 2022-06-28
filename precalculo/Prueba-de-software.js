39b78936300a204b00e2bf43318de2aa97ea444f
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-28 04:35:21 -0400

    prueba de software class 27

--- precalculo/Prueba-de-software.js
+++ precalculo/Prueba-de-software.js
@@ -168,6 +168,22 @@ Establecer breakpoints
 Diseñar una matrix de pruebas
 Establecer los datos de prueba
 Comenzar a depurar */
-/* */
+/*27 atomatizacion de pruebas 
+
+En esta unidad vamos a conocer las bases para la automatización de pruebas y podemos automatizar las siguientes tipos de pruebas.
+
+Pruebas unitarias: Tienen que ver con un pedazo de código que el desarrollador esta codificando, pero no tienen que ver con todo el flujo de negocio y proceso del software.
+
+Pruebas de integración: Cómo hacemos que el conjunto del equipo que libera pedacitos de software funcionen juntos y no hagan defectos adicionales.
+
+Pruebas funcionales o de aceptación: Estas pruebas no necesariamente forman parte de los requerimientos especificados por el cliente, una recomendación para automatizar estas pruebas es que deban cumplir con los requerimientos dados por el cliente.
+
+Test Driven Development: El desarrollo va a estar enfocado haciendo primera las pruebas y después el código. Haciendo que el desarollo sea muy específico con la mayor cobertura y no pongamos líneas de código que no van a funcionar o no se usan.
+
+Escribimos una prueba
+Ejecutamos la prueba: Falla
+Se escribe el código
+Ejecutamos la prueba: Pasa
+Behavior Driven Development: Si primeros vamos a escribir las pruebas, debemos hacerlo bien y usando un lenguaje sencillo, simple para que la sirva al equipo para entender qué es lo que queremos hacer. */
 /* */
 /* */
