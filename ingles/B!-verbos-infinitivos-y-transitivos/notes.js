7a2e73d82415177c6269d7dae915c0f7b9ca3a26
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-28 06:05:44 -0400

    ingles b1 infin.. and transit... class 10-2

--- ingles/B!-verbos-infinitivos-y-transitivos/notes.js
+++ ingles/B!-verbos-infinitivos-y-transitivos/notes.js
@@ -1,7 +1,29 @@
 /*10 Until and till
 
+Let's sort this out once and for all:
+    is there a difference between until and till ?
 
+Untill :
+    es una preposición que expresa que algo sucederá hasta un momento dado. Es decir, alguna acción ha comenzado y terminará en el momento mencionado.
 
+    For example, we may say:
+
+    1. Can you wait until I finish eating my lunch ?
+    ¿Puedes esperar hasta que termine de comer mi almuerzo?
+
+    2. The show will be live until midnight.
+    El programa estará en vivo hasta la medianoche.
+
+Till :
+    se puede usar de la misma manera que Untill, pero se percibe como menos formal. Esta formalidad es realmente la única diferencia entre las dos palabras.
+
+    Some sentences in which we might use till are:
+
+    1. I'm going out till the sun comes out.
+    Voy a salir hasta que salga el sol.
+
+    2. I'll be out of town till tomorrow.
+    Estaré fuera de la ciudad hasta mañana.
 */
 /* */
 /* */
