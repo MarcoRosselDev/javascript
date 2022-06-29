67aa42d8319e6c6bdced1c82b1dcd55b66b1a8dd
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-29 07:05:30 -0400

    node.js MongoDB y WebSockets class 7

--- 21-node-js-base-de-datos/server.js
+++ 21-node-js-base-de-datos/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function(request, response){
-    response.send('Hola!');
+app.use(router);
+
+router.get('/', function (request, response) {
+    response.send('Hola desde get');
 });
+
+// app.use('/', function(request, response){
+//     response.send('Hola!');
+// });
 
 app.listen(3000);
 console.log('La aplicación esta escuchando en el puerto 3000');
