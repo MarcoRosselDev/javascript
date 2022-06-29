e045b58f53af475be77b2281f6df72358b756aed
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-29 06:11:08 -0400

    node.js MongoDB y WebSockets class 4

---
+++ 21-node-js-base-de-datos/class.js
@@ -0,0 +1,31 @@
+/*Métodos, cabeceras y estados
+
+Métodos HTTP:
+
+GET: Recoger información del servidor.
+POST: Añadir información al servidor.
+PUT: Reemplazar información en el servidor.
+PATCH: Actualizar parte de la información.
+DELETE: Eliminar información del servidor.
+OPTIONS: Pedir información sobre métodos (saber si podemos ejecutar alguno de los métodos anteriores).
+Las cabeceras serán el envío al servidor de cómo queremos hacer la petición.
+
+Los estados son números que indica el estado de la petición:
+
+2XX: Todo ha ido bien.
+3XX: La petición se ha redirigido.
+4XX: Errores del cliente.
+5XX: Ha habido un error al procesar la petición.
+
+*/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
