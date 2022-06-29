fb3e3e212fca0b2e0da04a24f49bc21e564e2b81
Marco Rossel <andresmarcorossel@gmail.com>
2022-06-29 08:19:00 -0400

    node.js MongoDB y WebSockets class 7-2

--- 21-node-js-base-de-datos/server.js
+++ 21-node-js-base-de-datos/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 var app = express();
-
+app.use(bodyParser.json());
 app.use(router);
 
 router.get('/', function (request, response) {
     response.send('Hola desde get');
+});
+
+router.delete('/', function (request, response) {
+    console.log(request.body);
+    response.send('Hola desde delete');
+});
+
+router.put('/', function (request, response) {
+    console.log(request.body);
+    response.send('Hola desde put');
 });
 
 // app.use('/', function(request, response){
