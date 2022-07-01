4ed831da6f023ab4bfb4c135bfabdbe4b7b6faf3
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-01 06:37:53 -0400

    javaScript avanzado class 7-2

--- 19-javaScript-avanzado/axm-js.js
+++ 19-javaScript-avanzado/axm-js.js
@@ -6,6 +6,20 @@
 9/Curso Básico de JavaScript 5/22
 11/Curso de Webpack 3/28
 12/Curso de Webpack 23/28
-14/
+14/Curso de Prework: Configuración de Entorno de Desarrollo en Linux 10/17
+
+18/Curso Práctico de React.js 2/30
+20/Curso Práctico de React.js 17/30
+21/Curso de Backend con Node.js: API REST con Express.js 18/25
+22/Curso de Backend con Node.js: Base de Datos con 
+PosgreSQL 4/27
+
+24/Curso de Backend con Node.js: Autenticación con Passport.js y JWT 17/20
+
+25/Curso Práctico de Next.js 21/22
+26/Curso Práctico de Next.js 14/22
+
+28/Curso profesional de Next.js 7/31
+30/Curso de Asincronismo con JavaScript 11/21
 
 */
