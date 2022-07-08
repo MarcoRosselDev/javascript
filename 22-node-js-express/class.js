1b87c88b3a2dc046b80651b25faa42b1c512b57c
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-08 15:49:41 -0400

    node.js Express.js class 1

---
+++ 22-node-js-express/class.js
@@ -0,0 +1,16 @@
+/*3
+3
+
+
+*/
+/*12
+16
+¿Qué parámetros debemos recibir para tener un middleware que lea errores?
+------------------------------------------------------
+solo el error xxxxxxx
+------------------------------------------------------
+-error, req, res y next
+-req, res y next
+-solo el error
+------------------------------------------------------
+*/
