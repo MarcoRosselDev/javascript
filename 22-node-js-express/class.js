fde7abba0c07d3edf7df2c3a80c8a4992a52abba
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-08 16:04:03 -0400

    node.js Express.js class 1-3

--- 22-node-js-express/class.js
+++ 22-node-js-express/class.js
@@ -1,6 +1,46 @@
 /*3
 3
+¿Cuál método de express nos sirve para retornar un formato json como respuesta?
+------------------------------------------------------
+app.sendJson({...})
+------------------------------------------------------
+------------------------------------------------------
 
+*/
+/*4
+6
+¿Cuál es la manera de  correcta de declarar una ruta que reciba un parámetro llamado productId?
+------------------------------------------------------
+app.get(’/products/<productid>’, …)
+------------------------------------------------------
+------------------------------------------------------
+
+*/
+/*5
+6
+¿Cuál la manera de  correcta obtener el parámetro productId enviado desde una ruta con GET?
+------------------------------------------------------
+const productId = req.param(' productId ');
+------------------------------------------------------
+------------------------------------------------------
+
+*/
+/*6
+7
+¿Cuál es la manera de  correcta obtener parámetros tipo query de una ruta con GET?
+------------------------------------------------------
+const { limit, offset } = req.queryParams;
+------------------------------------------------------
+------------------------------------------------------
+
+*/
+/*8
+10
+¿Cuál es el middleware de express que me permite recibir la información de POST en formato json?
+------------------------------------------------------
+app.use(express.jsonApply())
+------------------------------------------------------
+------------------------------------------------------
 
 */
 /*10
