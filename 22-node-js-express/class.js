bf3de33dceecdb60734f9540e46a1c64c5240d49
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-08 15:54:25 -0400

    node.js Express.js class 1-2

--- 22-node-js-express/class.js
+++ 22-node-js-express/class.js
@@ -3,6 +3,18 @@
 
 
 */
+/*10
+12
+¿Cuál es la forma correcta enviar un error de tipo 404 con una repsuesta tipo json?
+------------------------------------------------------
+res.statusCode(404).json({message})
+------------------------------------------------------
+-res.httpStatus(404).json({message})
+-res.status(404).json({message})
+-res.statusCode(404).json({message})
+------------------------------------------------------
+
+*/
 /*12
 16
 ¿Qué parámetros debemos recibir para tener un middleware que lea errores?
