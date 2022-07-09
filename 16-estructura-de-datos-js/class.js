60373520abd2ae80b858841693e8bd4b7e90222c
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-08 23:05:44 -0400

    node.js PosgreSQL class 1-5

--- 16-estructura-de-datos-js/class.js
+++ 16-estructura-de-datos-js/class.js
@@ -2,6 +2,12 @@
 ------------------------------------------------------
 8 Bytes
 ------------------------------------------------------
+a
+1 Byte
+b
+4 Bytes
+c
+8 Bytes
 */
 /*5 ¿Cuál es el patrón de un Stack?
 ------------------------------------------------------
@@ -22,6 +28,12 @@ Los nodos de la izquierda deben de ir en incremento y los de la derecha deben de
 ------------------------------------------------------
 Es la forma en que se guarda en memoria la información de un grafo.
 ------------------------------------------------------
+a
+Es la forma en que se guarda en memoria la información de un grafo.
+b
+Es un borde o puntero que conecta dos nodos.
+c
+Son los Nodos de información que van creando nuestro grafo.
 */
 /*11 ¿Qué es un “Vertex” en un grafo?
 ------------------------------------------------------
@@ -32,19 +44,43 @@ Es un patrón para buscar más rápido la información en un grafo.
 ------------------------------------------------------
 Es un grafo dirigido que para cada vértice, no hay un camino directo que empiece y termine en esa vértice.
 ------------------------------------------------------
+a
+Es un Grafo donde los bordes que conectan cada nodo son bidireccionales.
+b
+Es un grafo que los bordes que conectan cada nodo sólo tienen una dirección
+c
+Es un grafo dirigido que para cada vértice, no hay un camino directo que empiece ytermine en esa vértice.
 */
 /*13 ¿Qué es un Grafo acíclico?
 ------------------------------------------------------
 Es un grafo dirigido que sin importar el vértice que tomes siempre hay un camino directo que empiece y termine en esa vértice.
 ------------------------------------------------------
+a
+Es un grafo dirigido que para cada vértice, no hay un camino directo que empiece ytermine en esa vértice.
+b
+Es un grafo dirigido que sin importar el vértice que tomes siempre hay un camino directoque empiece y termine en esa vértice.
+c
+Es un grafo que los bordes que conectan cada nodo son bidireccionales
 */
 /*14 ¿Cuál es el nombre que reciben los siguientes niveles de un Binary Tree?
 ------------------------------------------------------
 Sub-Parents
 ------------------------------------------------------
+a
+Sub-Parents
+b
+Leafs
+c
+Siblings
 */
 /*16 ¿Para qué utilizamos el método de dequeue?
 ------------------------------------------------------
 Agregar un elemento al final de la línea
 ------------------------------------------------------
+a
+Agregar un elemento al final de la línea
+b
+Tomar el primer elemento de la línea
+c
+Remover al primer elemento de la línea
 */
