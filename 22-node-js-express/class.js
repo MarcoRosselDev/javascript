4105a435fec5f3c9deeafcd9382877c0b3ca1f5f
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-08 20:51:01 -0400

    node.js Express.js class 2-4

--- 22-node-js-express/class.js
+++ 22-node-js-express/class.js
@@ -69,8 +69,8 @@ app.use(express.json());
 res.statusCode(404).json({message})
 ------------------------------------------------------
 -res.httpStatus(404).json({message})
--res.status(404).json({message})
--res.statusCode(404).json({message})
+-res.status(404).json({message}) ++++++++++++++++++++
+-res.statusCode(404).json({message})-----
 ------------------------------------------------------
 
 */
