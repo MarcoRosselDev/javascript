57e55b9dd4336e1d1bc4edcb5890e2b15338af54
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-09 15:31:05 -0400

    intro-React class 1-3

--- 30-intro-React/class.js
+++ 30-intro-React/class.js
@@ -19,20 +19,32 @@ Para teletransportar componentes a un nodo de HTML distinto al nodo donde hace r
 Una sintaxis especial de HTML para escribir elementos y componentes de React que se siente como JavaScript.
 -------------------------------------------
 */
-/*
+/*6
+5
+¿Qué son las props en React?
 -------------------------------------------
--------------------------------------------
-*/
-/*
--------------------------------------------
--------------------------------------------
-*/
-/*
--------------------------------------------
+La forma en que React guarda información de nuestro componente para escuchar cuando tenga cambios y disparar un nuevo render.
 -------------------------------------------
 */
-/*
+/*7
+5
+¿Cuál es la diferencia entre componentes y elementos en React?
 -------------------------------------------
+Los elementos son grupos de componentes, reciben props y pueden crear estado o efectos. Los componentes reciben atributos o eventos y (casi siempre) se transforman en etiquetas de HTML.
+-------------------------------------------
+*/
+/*9
+22
+¿Por qué debemos compilar nuestro proyecto con React.js antes de subirlo a GitHub Pages?
+-------------------------------------------
+No es obligatorio compilar nuestro proyecto antes de subirlo a GitHub Pages.
+-------------------------------------------
+*/
+/*10
+9
+¿Cuál de las siguientes es una forma VÁLIDA de crear un estado en React?
+-------------------------------------------
+const nombreDelEstado = React.useState("valor inicial de estado"); const setNombreDelEstado = nombreDelEstado.setState();
 -------------------------------------------
 */
 /*
