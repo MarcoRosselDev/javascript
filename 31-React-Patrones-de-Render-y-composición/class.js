b9830c5d910b644194ddacbf7a75c083c620d118
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-09 22:22:54 -0400

    React Patrones de Render class 1

---
+++ 31-React-Patrones-de-Render-y-composición/class.js
@@ -0,0 +1,75 @@
+/*3
+2
+¿Qué son los principios de diseño?
+------------------------------------------
+Los colores, tamaños, fuentes y espaciados que usa un equipo de diseño para diseñar.
+------------------------------------------
+*/
+/*7
+2
+¿Qué es el código aburrido? ¿Es mejor que el código elegante?
+------------------------------------------
+No, el código elegante es mejor que el código aburrido porque causa una mejor impresión de nuestro trabajo.
+------------------------------------------
+*/
+/*8
+3
+¿Qué principios podemos seguir para decidir dónde colocar nuestro estado?
+------------------------------------------
+Máxima cercanía a la relevancia o stateful vs. stateless components, pero no ambos porque son incompatibles.
+------------------------------------------
+*/
+/*9
+3
+Tienes un componente App que por dentro tiene un componente TodoHeader que por dentro tiene un componente TodoCounter. ¿Cómo envías props desde App hasta TodoCounter sin usar React Context ni pasar esas mismas props por el componente TodoHeader?
+------------------------------------------
+Con React Portals.
+------------------------------------------
+*/
+/*10
+3
+¿Cómo identificas a un componente que aplica composición de uno que no la aplica?
+------------------------------------------
+Los componentes que SÍ aplican composición SÍ definen su contenido interno. Los componentes que NO la aplican le delegan esa responsabilidad al componente que los llame.
+------------------------------------------
+*/
+/*11
+8
+¿Cuál de los siguientes bloques de código es un ejemplo de render props?
+------------------------------------------
+function Componente(props) { return / … /; }
+const ComponenteWithHOC = withHoc(Componente);
+------------------------------------------
+*/
+/*
+------------------------------------------
+------------------------------------------
+*/
+/*
+------------------------------------------
+------------------------------------------
+*/
+/*
+------------------------------------------
+------------------------------------------
+*/
+/*
+------------------------------------------
+------------------------------------------
+*/
+/*
+------------------------------------------
+------------------------------------------
+*/
+/*
+------------------------------------------
+------------------------------------------
+*/
+/*
+------------------------------------------
+------------------------------------------
+*/
+/*
+------------------------------------------
+------------------------------------------
+*/
