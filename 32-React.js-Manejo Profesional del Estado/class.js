3695b8a982fdf362d5ed1a0225ad53aa26b209f0
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-11 16:28:20 -0400

    React-Manejo prof del estado class 1

---
+++ 32-React.js-Manejo Profesional del Estado/class.js
@@ -0,0 +1,35 @@
+/*1
+16
+¿Cuál de las afirmaciones es INCORRECTA sobre los estados derivados?
+----------------------------------------
+Se actualizan automáticamente con los cambios a los estados "normales".
+----------------------------------------
+*/
+/*2
+13
+¿Cuál es la técnica más popular para crear un reducer?
+----------------------------------------
+Reducer Objects
+----------------------------------------
+*/
+/*5
+5
+¿Cuál de los siguientes métodos NO PERTENECE al ciclo de vida de los componentes creados con React.Component?
+----------------------------------------
+UNSAFE_componentWillMount
+----------------------------------------
+*/
+/*8
+14
+Si creamos un estado patito con React.useState, a su actualizador por convención lo llamaremos setPatito. ¿Cómo se llamaría el actualizador de un estado patito si lo creamos con React.useReducer?
+----------------------------------------
+updatePatito
+----------------------------------------
+*/
+/*9
+15
+¿Para qué sirven los action creators?
+----------------------------------------
+Para unir el código de interfaz con el código de manejo del estado.
+----------------------------------------
+*/
