72a02a175370d04fa05f37417880b93c5f266b1e
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-11 23:07:35 -0400

    React-Manejo prof del estado class 2-3

--- 32-React.js-Manejo Profesional del Estado/class.js
+++ 32-React.js-Manejo Profesional del Estado/class.js
@@ -1,17 +1,15 @@
-/*1
+/*1 +++
 16
 ¿Cuál de las afirmaciones es INCORRECTA sobre los estados derivados?
 ----------------------------------------
 Se actualizan automáticamente con los cambios a los estados "normales".
 ----------------------------------------
 a
-Pueden tener la misma convención de nombres que los estados "normales".
+Pueden tener la misma convención de nombres que los estados "normales". +
 b
-Se crean a partir de estados "normales".
+Se crean a partir de estados "normales". +
 c
-Se crean con React.useState.
-d
-Se actualizan automáticamente con los cambios a los estados "normales".
+Se crean con React.useState. ++++++++++++++++++++++++++++++++++++
 */
 /*2 +++
 13
@@ -39,35 +37,31 @@ componentDidUpdate +
 e
 componentDidUnmount +++++++++++++++++++++++++
 */
-/*8
+/*8 +++
 14
 Si creamos un estado patito con React.useState, a su actualizador por convención lo llamaremos setPatito. ¿Cómo se llamaría el actualizador de un estado patito si lo creamos con React.useReducer?
 ----------------------------------------
 updatePatito
 ----------------------------------------
 a
-dispatch
+dispatch ++++++++++++++++++
 b
 patitoUpdater
 c
 setPatito
-d
-updatePatito
 e
 setState
 */
-/*9
+/*9 +++
 15
 ¿Para qué sirven los action creators?
 ----------------------------------------
 Para unir el código de interfaz con el código de manejo del estado.
 ----------------------------------------
 a
-Para guardar todos los action types en un mismo objeto y evitar errores de typos.
+Para guardar todos los action types en un mismo objeto y evitar errores de typos. +++++++++++++++++++++++++++++++++++++++
 b
 Para separar el código de interfaz del código de manejo del estado.
-c
-Para unir el código de interfaz con el código de manejo del estado.
 */
 let array = {
     1:16,
