5ac0f16fcf47ce5e642aff237bcbf179a33e5de4
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-13 18:42:39 -0400

    Next.js practico class 3

---
+++ ingles/B2-Presente Perfecto y Preposiciones/class.js
@@ -0,0 +1,77 @@
+/*1 -
+3
+In the sentence: "Charles has gone to Paris", where is Charles?
+-----------------------------------------
+We don't know.
+-----------------------------------------
+a
+At home.
+b
+We don't know.
+c
+At work.
+d
+In Paris.
+*/
+/*5
+6
+"Prefixes are added at the end of the words", this is:
+-----------------------------------------
+Sometimes
+-----------------------------------------
+*/
+/*6
+7
+The suffix -less, means:
+-----------------------------------------
+Capacity
+-----------------------------------------
+*/
+/*9
+10
+Select the sentence where the word "YOU" is used in an impersonal form.
+-----------------------------------------
+You did great. Well done!
+-----------------------------------------
+*/
+/*10
+11
+Is this sentence correct or incorrect? "She works like a teacher".
+-----------------------------------------
+Correct
+-----------------------------------------
+*/
+/*11
+11
+In the sentence: "As your father, I should tell you that it wasn't a good idea to use the car without permission", who am I?
+-----------------------------------------
+I'm your friend.
+-----------------------------------------
+*/
+/*13
+12
+Is this sentence correct or incorrect? "Who did you go to the party with?"
+-----------------------------------------
+Incorrect
+-----------------------------------------
+*/
+/*19 -
+19
+Is it correct to say: "I am agree with you"?
+-----------------------------------------
+Sometimes, depending on the situation.
+-----------------------------------------
+a
+Sometimes, depending on the situation.
+b
+Not at all.
+c
+Yes, of course.
+*/
+/*20
+20
+Report the order: Please, call me back as soon as possible.
+-----------------------------------------
+He told me to call you back as soon as possible.
+-----------------------------------------
+*/
