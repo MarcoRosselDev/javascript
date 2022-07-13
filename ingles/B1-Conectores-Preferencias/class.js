ce29bd3381c113f37df266e274dfd42887f0617c
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-13 01:14:42 -0400

    Next.js practico class 2-2

--- ingles/B1-Conectores-Preferencias/class.js
+++ ingles/B1-Conectores-Preferencias/class.js
@@ -18,12 +18,18 @@ saw
 c
 see
 */
-/*10
+/*10 -
 13
 Which of the following sentences is incorrect?
 --------------------------------------------
 Jane is a cousin of hers.
 --------------------------------------------
+a
+Jane is a cousin of Mary’s.
+b
+Jane is a cousin of hers.
+c
+Jane is a cousin of Mary.
 */
 /*11
 14
@@ -32,29 +38,47 @@ Jane is a cousin of hers.
 Sure. I won't tell nobody!
 --------------------------------------------
 */
-/*12
+/*12 -
 16
 Complete the sentence: Your sister is ____ the ____ age ____ mine. Yours is 17 and mine 18
 --------------------------------------------
 not / same / to
 --------------------------------------------
+a
+- / same / from
+b
+not / same / as
+c
+not / same / to
 */
-/*13
+/*13 -
 17
 "Would you like to eat Thai or Italian food?" Select the best answer.
 --------------------------------------------
 I like more Italian than Thai.
 --------------------------------------------
+a
+I like more Italian than Thai.
+b
+I like Italian than Thai better.
+c
+I like Italian better than Thai
 */
 /*16 Complete the sentence: "No, I don't want to order food. _______ cooking some pasta?".
 --------------------------------------------
 Shall we
 --------------------------------------------
 */
-/*20
+/*20 -
 23
 I don't want to make dinner, you know I don't cook ___________.
 --------------------------------------------
 enough well
 --------------------------------------------
+a
+good enough
+b
+well enough
+c
+enough well
 */
