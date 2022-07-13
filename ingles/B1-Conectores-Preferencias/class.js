1b8d89a427e663868e16fcaecb429afc46f9967a
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-13 01:08:05 -0400

    Next.js practico class 2

---
+++ ingles/B1-Conectores-Preferencias/class.js
@@ -0,0 +1,60 @@
+/*1
+2
+What structure is used to describe live events?
+--------------------------------------------
+Present continuous
+--------------------------------------------
+*/
+/*5 -
+6
+Complete the sentence: "I _______ the great of China".
+--------------------------------------------
+see
+--------------------------------------------
+a
+have seen
+b
+saw
+c
+see
+*/
+/*10
+13
+Which of the following sentences is incorrect?
+--------------------------------------------
+Jane is a cousin of hers.
+--------------------------------------------
+*/
+/*11
+14
+"Can I tell you a secret?" Select the best answer.
+--------------------------------------------
+Sure. I won't tell nobody!
+--------------------------------------------
+*/
+/*12
+16
+Complete the sentence: Your sister is ____ the ____ age ____ mine. Yours is 17 and mine 18
+--------------------------------------------
+not / same / to
+--------------------------------------------
+*/
+/*13
+17
+"Would you like to eat Thai or Italian food?" Select the best answer.
+--------------------------------------------
+I like more Italian than Thai.
+--------------------------------------------
+*/
+/*16 Complete the sentence: "No, I don't want to order food. _______ cooking some pasta?".
+--------------------------------------------
+Shall we
+--------------------------------------------
+*/
+/*20
+23
+I don't want to make dinner, you know I don't cook ___________.
+--------------------------------------------
+enough well
+--------------------------------------------
+*/
