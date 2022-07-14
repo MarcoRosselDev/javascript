ea2c4b65f203676911cf804408e41441407e2e5c
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-14 02:26:05 -0400

    Next.js practico class 4-7

--- ingles/B2-Pronombres y Cláusulas Relativas/class.js
+++ ingles/B2-Pronombres y Cláusulas Relativas/class.js
@@ -1,9 +1,11 @@
-/*1
+/*1 +++
 3
 Dependent clauses can be considered sentences?
 -------------------------------------------
 TRUE
 -------------------------------------------
+a
+FALSE ++++++++++++++++++
 */
 /*3
 4
@@ -34,12 +36,20 @@ misunderstood
 d
 ununderstood
 */
-/*11
+/*11 ---
 12
 My boss didn't ______ me leave early.
 -------------------------------------------
 make
 -------------------------------------------
+a
+make
+b
+get
+c
+have
+d
+let
 */
 /*20
 21
@@ -48,10 +58,14 @@ We weren't __________ to leave the room.
 allowing
 -------------------------------------------
 */
-/*22
+/*22 ---
 22
 The presentation was ________ by Mike.
 -------------------------------------------
 gives
 -------------------------------------------
+a gave
+b given
+c give
+d gives
 */
