051243b7734b9497902dd7207945f8636e3ceacb
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-15 23:27:51 -0400

    Next.js practico class 7-7

--- ingles/B2-Adjetivos y Voz Pasiva/class.js
+++ ingles/B2-Adjetivos y Voz Pasiva/class.js
@@ -1,14 +1,30 @@
-/*3--3
+/*3--3 +++
 Choose the sentence in Passive Voice.
 --------------------------------------
 People have celebrated Christmas in many countries of the world.
 --------------------------------------
+a
+People have celebrated Christmas in many countries of the world.
+b
+People celebrating Christmas in many countries of the world.
+c
+People celebrate Christmas in many countries of the world.
+d
+Christmas is celebrated in many countries of the world.
 */
-/*8--8
+/*8--8 +++
 Choose the correct tag question for this sentence: You are my sister's friend, ___________ ?
 --------------------------------------
 you aren't?
 --------------------------------------
+a
+is you?
+b
+you aren't?
+c
+aren't you?
+d
+are you?
 */
 /*12--12
 Choose the correct negative suggestion.
@@ -22,11 +38,17 @@ Choose the sentence with the correct adjective order:
 None of the answers are correct.
 --------------------------------------
 */
-/*16--15
+/*16--15 +++
 Choose the correct adjective order.
 --------------------------------------
 None of the answers are correct.
 --------------------------------------
+b
+Opinion, age, shape, size, origin.
+c
+Opinion, size, shape, age, origin.
+d
+Opinion, shape, size, origin, age.
 */
 /*18--18
 When we say - Tacos from this restaurant are quite delicious.
