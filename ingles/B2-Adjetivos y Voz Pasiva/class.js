3a894c5cf69c1b076c6c2f4e4f7728a0a6bece21
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-15 23:21:05 -0400

    Next.js practico class 7-6

---
+++ ingles/B2-Adjetivos y Voz Pasiva/class.js
@@ -0,0 +1,48 @@
+/*3--3
+Choose the sentence in Passive Voice.
+--------------------------------------
+People have celebrated Christmas in many countries of the world.
+--------------------------------------
+*/
+/*8--8
+Choose the correct tag question for this sentence: You are my sister's friend, ___________ ?
+--------------------------------------
+you aren't?
+--------------------------------------
+*/
+/*12--12
+Choose the correct negative suggestion.
+--------------------------------------
+Let's not have mentioned this to our boss.
+--------------------------------------
+*/
+/*14--15
+Choose the sentence with the correct adjective order:
+--------------------------------------
+None of the answers are correct.
+--------------------------------------
+*/
+/*16--15
+Choose the correct adjective order.
+--------------------------------------
+None of the answers are correct.
+--------------------------------------
+*/
+/*18--18
+When we say - Tacos from this restaurant are quite delicious.
+--------------------------------------
+Quite in this case means 'little but not very'.
+--------------------------------------
+*/
+/*19--19
+He works _________ the best researchers in the country.
+--------------------------------------
+‘among’ or ‘between’ can be used here.
+--------------------------------------
+*/
+/*20--3
+Choose the sentence in Passive Voice.
+--------------------------------------
+Thousands of people are attending this music festival.
+--------------------------------------
+*/
