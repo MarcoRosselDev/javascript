7f6611a041d0427351e00361d9d0420ff686cfde
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-17 05:28:18 -0400

    TypeScript all class 2-5

--- 25-tailwind-1/class.js
+++ 25-tailwind-1/class.js
@@ -15,32 +15,42 @@ margin-top, relative, object-cover
 @component
 --------------------------------
 */
-/*
+/*7--23
+¿Qué variante se utiliza para cambiar el color de fondo a blanco cuando un elemento está seleccionado?
 --------------------------------
---------------------------------
-*/
-/*
---------------------------------
+select:bg-white
 --------------------------------
 */
-/*
+/*9 ¿Cómo le damos un formato redondo grande en las esquinas de nuestros elementos?
 --------------------------------
---------------------------------
-*/
-/*
---------------------------------
+rounded-big
 --------------------------------
 */
-/*
+/*10 ¿Cómo cambiamos nuestros elementos de vertical a horizontal en pantalla mediana usando flex?
 --------------------------------
---------------------------------
-*/
-/*
---------------------------------
+flex md:inline
 --------------------------------
 */
-/*
+/*12--6
+¿Cuáles son instrucciones válidas para cambiar de color a un elemento?
 --------------------------------
+shadow-gray-300 text-black outline-yellow-500
+--------------------------------
+*/
+/*13--8
+¿Cuál conjunto de instrucciones nos ayuda a tener la misma distancia en todos los lados de un elemento?
+--------------------------------
+mx-auto mr-2 mt-0
+--------------------------------
+*/
+/*14 ¿Cómo podemos cambiar el tamaño de letra a extra grande?
+--------------------------------
+font-xl
+--------------------------------
+*/
+/*15 ¿Cómo logramos que el espacio entre líneas sea amplio?
+--------------------------------
+line-height-xl
 --------------------------------
 */
 /*
