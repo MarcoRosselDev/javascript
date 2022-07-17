2f0d123c8a0325bcdd1e7ae8369abe99efd41233
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-17 05:41:39 -0400

    TypeScript all class 2-7

--- 25-tailwind-1/class.js
+++ 25-tailwind-1/class.js
@@ -14,16 +14,37 @@ Porque se utiliza con instrucciones directo en el HTML.
 e
 Porque modifi ca el DOM.
 */
-/*4--26
+/*+4--26
 ¿Qué se utiliza para fijar el navbar encima de otros elementos?
 --------------------------------
 margin-top, relative, object-cover
 --------------------------------
+a
+left-0, right-0, top-0, z-10
+b
+top-0, w-screen
+c
+fi
+xed, z-50, w-full
+d
+margin-top, relative, object-cover
+e
+absolute, left-0, top-0
 */
-/*5 ¿Qué instrucción o directiva se utiliza para crear componentes?
+/*+5 ¿Qué instrucción o directiva se utiliza para crear componentes?
 --------------------------------
 @component
 --------------------------------
+a
+@apply
+b
+@tailwind
+c
+@screen
+d
+@component
+e
+@responsive
 */
 /*7--23
 ¿Qué variante se utiliza para cambiar el color de fondo a blanco cuando un elemento está seleccionado?
@@ -31,21 +52,31 @@ margin-top, relative, object-cover
 select:bg-white
 --------------------------------
 */
-/*9 ¿Cómo le damos un formato redondo grande en las esquinas de nuestros elementos?
+/*+9 ¿Cómo le damos un formato redondo grande en las esquinas de nuestros elementos?
 --------------------------------
 rounded-big
 --------------------------------
+a rounded-all
+b rounded-l
+c corner-lg
+d rounded-big
+e rounded-lg
 */
 /*10 ¿Cómo cambiamos nuestros elementos de vertical a horizontal en pantalla mediana usando flex?
 --------------------------------
 flex md:inline
 --------------------------------
 */
-/*12--6
+/*+12--6
 ¿Cuáles son instrucciones válidas para cambiar de color a un elemento?
 --------------------------------
 shadow-gray-300 text-black outline-yellow-500
 --------------------------------
+a shadow-gray-300 text-black outline-yellow-500
+b flex-red-300 flex-col-red my-300
+c mt-white px-black bottom-yellow
+d w-gray-300 ml-red-500 px-blue-100
+e bg-gray-200 text-purple-700 border-blue-500
 */
 /*13--8
 ¿Cuál conjunto de instrucciones nos ayuda a tener la misma distancia en todos los lados de un elemento?
