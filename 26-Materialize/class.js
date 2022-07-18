e7ac6a5021a8ddf2a31f365eb52866b9d747af60
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-18 01:23:25 -0400

    Materialize class 1

---
+++ 26-Materialize/class.js
@@ -0,0 +1,24 @@
+/*5--8
+Cuando hablamos de la clase .row, ¿a qué hacemos referencia?
+----------------------------------
+Hacemos referencia a las columnas que se formarán en nuestro sistema de cuadrícula
+----------------------------------
+*/
+/*8--10
+¿La clase .container en el proyecto del curso es necesaria?
+----------------------------------
+Es una clase que forma parte de la estructura principal de los estilos de materialize, y si no está, no cargan otros estilos
+----------------------------------
+*/
+/*9--11
+¿De qué forma puedo alinear de forma vertical los elementos de un contenedor?
+----------------------------------
+Agregando la clase de vertical-align
+----------------------------------
+*/
+/*10--11
+¿Con qué clase puedo comenzar a agregar sombra a mis tarjetas o elementos de Materialize?
+----------------------------------
+.depth-shadw-1 a 5
+----------------------------------
+*/
