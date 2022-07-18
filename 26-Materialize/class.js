3329a4c1cc0a1adc544e15070cea3ba666f967cd
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-18 01:26:48 -0400

    Materialize class 1-2

--- 26-Materialize/class.js
+++ 26-Materialize/class.js
@@ -1,24 +1,54 @@
-/*5--8
+/*//5--8
 Cuando hablamos de la clase .row, ¿a qué hacemos referencia?
 ----------------------------------
 Hacemos referencia a las columnas que se formarán en nuestro sistema de cuadrícula
 ----------------------------------
+a
+Hacemos referencia a un contenedor que nos ayudará a centrar el contenido de nuestroproyecto
+b
+Hacemos referencia a las columnas que se formarán en nuestro sistema de cuadrícula
+c
+Hacemos referencia a un contenedor que generará una fi la dentro de nuestro sistema decuadrícula
 */
-/*8--10
+/*//8--10
 ¿La clase .container en el proyecto del curso es necesaria?
 ----------------------------------
 Es una clase que forma parte de la estructura principal de los estilos de materialize, y si no está, no cargan otros estilos
 ----------------------------------
+a
+No, y no es bueno utilizarla ya que crea problemas con alinear elementos
+b
+Es una clase que forma parte de la estructura principal de los estilos de materialize, y sino está, no cargan otros estilos
+c
+Sí, porque es la clase que inicia el Grid de Materialize
+d
+No, pero nos ayuda a centrar el contenido de nuestro proyecto
 */
-/*9--11
+/*//9--11
 ¿De qué forma puedo alinear de forma vertical los elementos de un contenedor?
 ----------------------------------
 Agregando la clase de vertical-align
 ----------------------------------
+a
+Agregando la clase de vertical-center
+b
+Agregando la clase de .valign-wrapper
+c
+Agregando la clase de vertical-align
+d
+Agregando la clase de .vertical-wrapper
 */
-/*10--11
+/*//10--11
 ¿Con qué clase puedo comenzar a agregar sombra a mis tarjetas o elementos de Materialize?
 ----------------------------------
 .depth-shadw-1 a 5
 ----------------------------------
+a
+.depth-shadw-1 a 5
+b
+.z-shadow
+c
+box-shadow
+d
+Con la clase de .z-depth-1 a 5
 */
