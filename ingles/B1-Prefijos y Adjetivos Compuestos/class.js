b6fd93624c55feadbfda06413c8523ef3ce25221
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-18 07:50:20 -0400

    ing B-1 prefijos class 1

---
+++ ingles/B1-Prefijos y Adjetivos Compuestos/class.js
@@ -0,0 +1,49 @@
+/*1..Complete the sentence: "This music is popular ________ college students"
+--------------------------------
+between
+--------------------------------
+*/
+/*4..Complete the sentence: "If you arrive too late, you ___________ to join the meeting".
+--------------------------------
+couldn't
+--------------------------------
+*/
+/*6..7
+Both "I hope she comes to the party" and "I hope she will come to the party" are correct sentences
+--------------------------------
+FALSE
+--------------------------------
+*/
+/*11..12|
+Report the following question: "Where was he?"
+--------------------------------
+He asked me where was he.
+--------------------------------
+a
+He asked me whether I was with him.
+b
+He asked me where he was.
+*/
+/*12..12
+Report the following question: "What did she do?"
+--------------------------------
+He asked me what she do
+--------------------------------
+*/
+/*13..14
+Complete the sentence: "I don't like ____ of those two movies".
+--------------------------------
+neither
+--------------------------------
+*/
+/*18..What verb can you make from the noun "Strength".
+--------------------------------
+Strang
+--------------------------------
+*/
+/*20..21
+Which of the following sentences is NOT correct?
+--------------------------------
+Pasta is the most delicious food she has eaten.
+--------------------------------
+*/
