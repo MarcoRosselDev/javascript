27e26caa1abcf799fc126e810b6fe3fe81269590
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-18 07:23:15 -0400

    Materialize class 2

--- 26-Materialize/class.js
+++ 26-Materialize/class.js
@@ -1,36 +1,39 @@
-/*//5--8+++
+/*//5--8
 Cuando hablamos de la clase .row, ¿a qué hacemos referencia?
 ----------------------------------
 Hacemos referencia a las columnas que se formarán en nuestro sistema de cuadrícula
-----------------------------------
+
 a
 Hacemos referencia a un contenedor que nos ayudará a centrar el contenido de nuestro proyecto
+----------------------------------
+
 c +++++
 Hacemos referencia a un contenedor que generará una fila dentro de nuestro sistema decuadrícula +++++++++++++
 */
-/*//8--10+++
+/*//8--10
 ¿La clase .container en el proyecto del curso es necesaria?
 ----------------------------------
 Es una clase que forma parte de la estructura principal de los estilos de materialize, y si no está, no cargan otros estilos
+
+Sí, porque es la clase que inicia el Grid de Materialize+++++++++++++++++++
 ----------------------------------
 a
 No, y no es bueno utilizarla ya que crea problemas con alinear elementos
-c
-Sí, porque es la clase que inicia el Grid de Materialize+++++++++++++++++++
+
 d
-No, pero nos ayuda a centrar el contenido de nuestro proyecto
+No, pero nos ayuda a centrar el contenido de nuestro proyecto +++
 */
-/*//9--11+++
+/*//9--11
 ¿De qué forma puedo alinear de forma vertical los elementos de un contenedor?
 ----------------------------------
 Agregando la clase de vertical-align
+
+Agregando la clase de .vertical-wrapper
 ----------------------------------
 a
-Agregando la clase de vertical-center
+Agregando la clase de vertical-center +++
 b
 Agregando la clase de .valign-wrapper
-d
-Agregando la clase de .vertical-wrapper +++
 */
 /*//10--11+++
 ¿Con qué clase puedo comenzar a agregar sombra a mis tarjetas o elementos de Materialize?
