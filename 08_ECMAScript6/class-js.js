34014f307cb4bf55efdce34187ddc1698fcfa582
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-18 23:17:16 -0400

    Materialize class 2-2

--- 08_ECMAScript6/class-js.js
+++ 08_ECMAScript6/class-js.js
@@ -3,6 +3,14 @@
 ----------------------------
 */
 /*14--13
+¿Para qué sirve el event.preventDefault?
 ----------------------------
+Para asegurarnos de que los eventos ejecuten su acción por defecto (por ejemplo, recargar la página al completar un formulario).
 ----------------------------
+a
+Para asegurarnos de que los eventos ejecutan la acción por defecto que les asignó elnavegador (en vez de código JS que hayamos escrito por error).
+b
+Para asegurarnos de que los eventos ejecuten su acción por defecto (por ejemplo,recargar la página al completar un formulario).
+c
+Para evitar que los eventos ejecuten su acción por defecto (por ejemplo, recargar lapágina al completar un formulario).
 */
