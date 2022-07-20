489572311a0abc1545f2413a85371fdc58dd4509
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-20 13:11:48 -0400

    ing B-1 prefijos class 3-3

--- ingles/B1-solicitudes y pronombres reflexivos/class.js
+++ ingles/B1-solicitudes y pronombres reflexivos/class.js
@@ -4,8 +4,10 @@ Complete the sentence: The car, ______ was blocking the way, is no longer there.
 who
 ------------------------------
 */
-/*
+/*11--14
+I'm learning Portuguese, and I'd like to get some advice from you on how best to do it. You can say:
 ------------------------------
+If I was you, I would take Platzi courses on Portuguese.
 ------------------------------
 */
 /*
