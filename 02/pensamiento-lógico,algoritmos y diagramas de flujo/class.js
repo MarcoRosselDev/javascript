d3b7475aca48cd68bd66a831364daf2f30a21441
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-21 18:16:35 -0400

    pensamiento logico class-1-3

--- 02/pensamiento-lógico,algoritmos y diagramas de flujo/class.js
+++ 02/pensamiento-lógico,algoritmos y diagramas de flujo/class.js
@@ -10,7 +10,9 @@ Verdadero
 Verdadero
 -------------------------------
 */
-/*
+/*13--15
+¿Qué tipos de score puede tener una función?
 -------------------------------
+Local o permanente
 -------------------------------
 */
