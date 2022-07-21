e64d8b7a62aa28001dda18710a5eb75e0f1d2c27
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-21 18:15:42 -0400

    pensamiento logico class-1-3

--- 02/pensamiento-lógico,algoritmos y diagramas de flujo/class.js
+++ 02/pensamiento-lógico,algoritmos y diagramas de flujo/class.js
@@ -4,8 +4,10 @@
 Verdadero
 -------------------------------
 */
-/*
+/*8--8
+¿La sentencia Catch de un bloque Try / Catch nos permite encapsular la sección de nuestro algoritmo en caso de que un resultado haya sido verdadero o sin errores?
 -------------------------------
+Verdadero
 -------------------------------
 */
 /*
