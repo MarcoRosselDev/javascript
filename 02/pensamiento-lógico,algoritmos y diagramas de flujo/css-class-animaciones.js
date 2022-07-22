6d0b64dc9266f17ea5c11b9e649883a3a2cd5f23
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-22 06:40:25 -0400

    css animation class 1-3

--- 02/pensamiento-lógico,algoritmos y diagramas de flujo/css-class-animaciones.js
+++ 02/pensamiento-lógico,algoritmos y diagramas de flujo/css-class-animaciones.js
@@ -4,3 +4,13 @@ Las propiedades indispensables para hacer una animación son:
 animation-name y animation-timing-function
 -------------------------------
 */
+/*5--5
+¿Cuál es la sintaxis correcta para declarar un keyframe?
+-------------------------------
+@keyframe .animation-name
+-------------------------------
+*/
+/*
+-------------------------------
+-------------------------------
+*/
