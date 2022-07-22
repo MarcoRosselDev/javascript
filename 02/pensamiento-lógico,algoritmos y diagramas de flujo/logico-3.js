c0a3ae6b078229a0fd0df00209937af8ddcee1af
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-22 13:22:48 -0400

    pensamiento logico class-2-2

--- 02/pensamiento-lógico,algoritmos y diagramas de flujo/logico-3.js
+++ 02/pensamiento-lógico,algoritmos y diagramas de flujo/logico-3.js
@@ -1,4 +1,7 @@
-/*
+/*9--12
+¿Cómo se empiezan a escribir las variables en PHP?
 ---------------------------
+Usando el operador = frente a cada variable
 ---------------------------
+con signo de pesos -- [$valor1 = 0;]
 */
