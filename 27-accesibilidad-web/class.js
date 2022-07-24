2b15905877101a1aeeed7a3ed52bbe8ffda2df7d
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-24 11:42:16 -0400

    accesibilidad web class 1-2

--- 27-accesibilidad-web/class.js
+++ 27-accesibilidad-web/class.js
@@ -1,3 +1,9 @@
+/*2--3
+¿Cómo se llama la iniciativa que se encarga de los estándares de accesibilidad de la web?
+----------------------------
+WAI - World a11y Internet
+----------------------------
+*/
 /*
 ----------------------------
 ----------------------------
