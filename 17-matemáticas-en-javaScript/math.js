3c82e63c683fdfc76e11bb3143b463ff8a25c02a
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-06 02:27:05 -0400

    js-math class 2

---
+++ 17-matemáticas-en-javaScript/math.js
@@ -0,0 +1,9 @@
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log({
+  ladoCuadrado, 
+  perimetroCuadrado, 
+  areaCuadrado
+  });
