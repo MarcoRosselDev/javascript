c1f2a87ee4a13966e1c9a7309da89158ab0252c9
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-06 22:26:40 -0400

    js-math class 3.4

--- 17-matemáticas-en-javaScript/project-Math/app.js
+++ 17-matemáticas-en-javaScript/project-Math/app.js
@@ -3,3 +3,23 @@
 2 include a button input
 3 innerHtml - print the result in the DOM 
  */
+ const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener('click', function(e){
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
